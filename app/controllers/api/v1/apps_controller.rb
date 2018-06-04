41f5952aedf0cb93d85a7a2d485b2b9102215eb4
Miguel Michelson <miguelmichelson@gmail.com>
2018-06-04 14:07:55 -0400

    location

--- app/controllers/api/v1/apps_controller.rb
+++ app/controllers/api/v1/apps_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::AppsController < ActionController::API
 
   def ping
     @app = App.find_by(key: params[:id])
-    data = resource_params.to_h
+    data = resource_params.to_h.deep_merge({"properties": request.location.data})
     ap = @app.add_visit(data)
     render json: ap
   end
