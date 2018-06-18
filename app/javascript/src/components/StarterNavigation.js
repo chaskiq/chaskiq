45e31f650010cdb9e5fc62977ccd28ae5a2d91aa
Miguel Michelson <miguelmichelson@gmail.com>
2018-06-18 00:58:30 -0400

    login dropdown

--- app/javascript/src/components/StarterNavigation.js
+++ app/javascript/src/components/StarterNavigation.js
@@ -10,6 +10,7 @@ import Nav, {
 import DashboardIcon from '@atlaskit/icon/glyph/dashboard';
 import GearIcon from '@atlaskit/icon/glyph/settings';
 import SearchIcon from '@atlaskit/icon/glyph/search';
+import WorldIcon from '@atlaskit/icon/glyph/world';
 import CreateIcon from '@atlaskit/icon/glyph/add';
 import AtlassianIcon from '@atlaskit/icon/glyph/atlassian';
 import ArrowleftIcon from '@atlaskit/icon/glyph/arrow-left';
@@ -19,6 +20,26 @@ import SearchDrawer from '../components/SearchDrawer';
 import HelpDropdownMenu from '../components/HelpDropdownMenu';
 import AccountDropdownMenu from '../components/AccountDropdownMenu';
 import atlaskitLogo from '../images/atlaskit.png';
+import { AkGlobalItem } from '@atlaskit/navigation';
+import Button from '@atlaskit/button';
+import Avatar from '@atlaskit/avatar';
+import SignOutIcon from '@atlaskit/icon/glyph/sign-out';
+
+import Dropdown, { DropdownItemGroup, DropdownItem } from '@atlaskit/dropdown-menu';
+
+const getDropDownData = () => (
+  <DropdownItemGroup title="Heading">
+    <DropdownItem>Hello it with some really quite long text here.</DropdownItem>
+    <DropdownItem>Some text 2</DropdownItem>
+    <DropdownItem isDisabled>Some disabled text</DropdownItem>
+    <DropdownItem>Some more text</DropdownItem>
+    <DropdownItem href="//atlassian.com" target="_new">
+      A link item
+    </DropdownItem>
+  </DropdownItemGroup>
+);
+
+
 
 export default class StarterNavigation extends React.Component {
   state = {
@@ -42,11 +63,24 @@ export default class StarterNavigation extends React.Component {
   render() {
     const backIcon = <ArrowleftIcon label="Back icon" size="medium" />;
     const globalPrimaryIcon = <AtlassianIcon label="Atlassian icon" size="xlarge" />;
+    const avatarIcon = <Dropdown trigger={ <Avatar
+                                            name={"miguel@preyhq.com"}
+                                            size="medium"
+                                            src={`https://api.adorable.io/avatars/24/${encodeURIComponent(
+                                              "miguel@preyhq.com",
+                                            )}.png`}
+                                          /> 
+                      }>
+                        {getDropDownData()}
+                      </Dropdown>
 
+    const logoutIcon = <SignOutIcon label="logout" size="medium"/>
+                        
     return (
       <Nav
         isOpen={this.context.navOpenState.isOpen}
         width={this.context.navOpenState.width}
+        isCollapsible
         onResize={this.props.onNavResize}
         containerHeaderComponent={() => (
           <AkContainerTitle
@@ -58,6 +92,7 @@ export default class StarterNavigation extends React.Component {
           />
         )}
         globalPrimaryIcon={globalPrimaryIcon}
+        globalSecondaryActions={[avatarIcon, logoutIcon ]}
         globalPrimaryItemHref="/"
         globalSearchIcon={<SearchIcon label="Search icon" />}
         hasBlanket
@@ -88,7 +123,8 @@ export default class StarterNavigation extends React.Component {
             />
           </AkCreateDrawer>
         ]}
-        globalAccountItem={AccountDropdownMenu}
+
+        globalAccountItem={<AccountDropdownMenu/>}
         globalCreateIcon={<CreateIcon label="Create icon" />}
         globalHelpItem={HelpDropdownMenu}
         onSearchDrawerOpen={() => this.openDrawer('search')}
