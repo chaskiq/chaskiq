9e84bc66fe4978f18b5201941ef36f2b266ff771
Miguel Michelson <miguelmichelson@gmail.com>
2018-06-29 04:33:15 -0400

    remove binding pry

--- app/services/fetch_link_card_service.rb
+++ app/services/fetch_link_card_service.rb
@@ -93,7 +93,7 @@ class FetchLinkCardService < BaseService
     card.type             = :link
     card.title            = meta_property(page, 'og:title') || page.at_xpath('//title').content
     card.description      = meta_property(page, 'og:description') || meta_property(page, 'description')
-    binding.pry
+
     card.image.attach( download_image(meta_property(page, 'og:image'))) if meta_property(page, 'og:image')
     
     return if card.title.blank?
