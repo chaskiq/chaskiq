c64fd85bfc029c986b3457ddda55a12f484cadd8
Miguel Michelson <miguelmichelson@gmail.com>
2019-05-15 16:46:11 -0400

    Update create_predicate.rb

--- app/graphql/mutations/predicates/create_predicate.rb
+++ app/graphql/mutations/predicates/create_predicate.rb
@@ -2,13 +2,26 @@ module Mutations
   class Predicates::CreatePredicate < GraphQL::Schema::RelayClassicMutation
     # TODO: define return fields
     # field :post, Types::PostType, null: false
+    field :app, Types::AppType, null: false
+    field :errors, Types::JsonType, null: true
+    
+    argument :app_params, Types::JsonType, required: true
+    argument :operation, String, required: false
 
-    # TODO: define arguments
-    # argument :name, String, required: true
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+    @segment = @app.segments.new(segment_params)
+
+
+    def resolve(app_params:, operation: )
+      current_user = context[:current_user]
+      if operation.blank? or operation == "new"
+        @app = current_user.apps.new
+      elsif 
+        @app = current_user.apps.create(app_params.permit!)        
+      end
+
+      { app: @app, errors: @app.errors }
+    end
   end
+
 end
