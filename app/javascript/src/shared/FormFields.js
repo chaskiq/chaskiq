d77f3a7fe70b0f59c55c09d7f6366443dae357b6
michelson <miguelmichelson@gmail.com>
2019-05-23 11:22:57 -0400

    material forms

--- app/javascript/src/shared/FormFields.js
+++ app/javascript/src/shared/FormFields.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import Select from '@atlaskit/select';
+//import Select from '@atlaskit/select';
 import FieldTextArea from '@atlaskit/field-text-area';
 import FieldText from '@atlaskit/field-text';
 import { DateTimePicker } from '@atlaskit/datetime-picker';
-import { Checkbox } from '@atlaskit/checkbox';
+//import { Checkbox } from '@atlaskit/checkbox';
 import { Field } from '@atlaskit/form';
 import { snakeCase, camelCase } from 'lodash'
+
+
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Input from '@material-ui/core/Input';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Select from '@material-ui/core/Select';
+import Chip from '@material-ui/core/Chip';
+
 
 export const errorsFor = (name, errors ) => {
   if (!errors[name])
@@ -16,7 +27,19 @@ export const errorsFor = (name, errors ) => {
 export const fieldRenderer = (namespace, data, props, errors) => {
   switch (data.type) {
     case "string":
-      return <Field label={data.name} isRequired
+      // className={classes.formControl}
+      return <FormControl>
+                <InputLabel htmlFor="component-simple">
+                  {data.name}
+                </InputLabel>
+                <Input id="component-simple" 
+                  value={props.data[camelCase(data.name)]} 
+                  //onChange={this.handleChange} 
+                />
+              </FormControl>
+
+
+      {/*<Field label={data.name} isRequired
         isInvalid={errorsFor(data.name, errors)}
         invalidMessage={errorsFor(data.name, errors)}>
         <FieldText name={`${namespace}[${data.name}]`}
@@ -26,20 +49,34 @@ export const fieldRenderer = (namespace, data, props, errors) => {
           minLength={data.maxLength}
           placeholder={data.placeholder}
         />
-      </Field>
+      </Field>*/}
 
     case "text":
 
-      return <Field label={data.name}
-        isInvalid={errorsFor(data.name, errors)}
-        invalidMessage={errorsFor(data.name, errors)}>
-        <FieldTextArea
-          name={`${namespace}[${data.name}]`}
-          shouldFitContainer
-          label={`${namespace}[${data.name}]`}
-          value={props.data[data.name]}
-        />
-      </Field>
+      return <FormControl>
+                <InputLabel htmlFor="component-simple">
+                  {data.name}
+                </InputLabel>
+                <Input id="component-simple"
+                  multiline={true}
+                  value={props.data[camelCase(data.name)]} 
+                  //onChange={this.handleChange} 
+                />
+              </FormControl>
+
+      {
+        /*<Field label={data.name}
+          isInvalid={errorsFor(data.name, errors)}
+          invalidMessage={errorsFor(data.name, errors)}>
+          <FieldTextArea
+            name={`${namespace}[${data.name}]`}
+            shouldFitContainer
+            label={`${namespace}[${data.name}]`}
+            value={props.data[data.name]}
+          />
+        </Field>
+        */
+      }
 
     case "datetime":
       return <Field label={data.name} isRequired
@@ -53,7 +90,8 @@ export const fieldRenderer = (namespace, data, props, errors) => {
         />
       </Field>
     case "select":
-      const name = data.multiple ? `${namespace}[${data.name}][]` : `${namespace}[${data.name}]`
+      
+      /*const name = data.multiple ? `${namespace}[${data.name}][]` : `${namespace}[${data.name}]`
       let defaultData = null
       if (data.multiple) {
         if (!props.data[data.name]){
@@ -90,12 +128,82 @@ export const fieldRenderer = (namespace, data, props, errors) => {
           })
           }
         />
-      </Field>
+      </Field>*/
+
+      let defaultData = null
+
+
+      if (data.multiple) {
+        if (!props.data[data.name]){
+          defaultData = {
+            label: data.default,
+            value: data.default
+          }
+        } else {
+          defaultData = props.data[data.name].map((o) => {
+            return {
+              label: o,
+              value: o
+            }
+          })
+        }
+      } else {
+        defaultData = {
+          label: props.data[data.name] || data.default,
+          value: props.data[data.name] || data.default
+        }
+      }
+
+      return <FormControl>
+        <InputLabel htmlFor="select-multiple-chip">
+          {data.name}
+        </InputLabel>
+        <Select
+          multiple={data.multiple}
+          name={name}
+          //value={defaultData}
+          //onChange={this.handleChange}
+          defaultValue={defaultData}
+          input={<Input id="select-multiple-chip" />}
+          renderValue={selected => {
+            debugger
+            return <div 
+              //className={classes.chips}
+            >
+              {selected.map(value => (
+                <Chip 
+                  key={value} 
+                  label={value} 
+                  //className={classes.chip} 
+                 />
+              ))}
+            </div>
+          }
+          }
+          //MenuProps={MenuProps}
+        >
+
+          {
+            data.options.map((o) => {
+              //return { label: o, value: o }
+              return <MenuItem 
+                        key={o} 
+                        value={o} 
+                         //style={getStyles(name, this)}
+                         >
+                        {o}
+                      </MenuItem>
+
+            })
+          }
+        </Select>
+      </FormControl>
+
     case "bool":
       return <div>
         <label>{data.name}</label>
-        <input
-          type="checkbox"
+        
+        <Checkbox
           defaultChecked={props.data[data.name]}
           name={`${namespace}[${data.name}]`}
         />
