e991fa2b44add01cb7d97df8311a9c8359e4793a
michelson <miguelmichelson@gmail.com>
2019-06-19 00:29:59 -0400

    client tester system spec

--- app/controllers/client_tester_controller.rb
+++ app/controllers/client_tester_controller.rb
@@ -7,7 +7,7 @@ class ClientTesterController < ApplicationController
   def show
 
     # open_uri("isReverse={true}")
-    key = App.first.encryption_key # SecureRandom.hex(8) o SecureRandom.random_bytes(16)
+    key = get_app.encryption_key # SecureRandom.hex(8) o SecureRandom.random_bytes(16)
     # payload = "miguel@preyhq.com"
 
     @h = {
@@ -18,8 +18,8 @@ class ClientTesterController < ApplicationController
     @json_payload = {
       domain: @h[:http] + request.env["HTTP_HOST"],
       ws: @h[:ws] + request.env["HTTP_HOST"]+ "/cable",
-      app_id: "#{App.first.key}",
-      email: "miguel@preyhq.com",
+      app_id: "#{get_app.key}",
+      email: "test@test.cl",
       properties: {
                     name: "miguel",
                     lang: "es",
@@ -30,5 +30,9 @@ class ClientTesterController < ApplicationController
                   }
     }.to_json
     @encrypted_data = JWE.encrypt(@json_payload, key, alg: 'dir')
+  end
+
+  def get_app
+    App.find_by(key: params[:id]) rescue App.first
   end
 end
