3d5dd43f189b822c7cf1c08666c3f3c070eb062b
michelson <miguelmichelson@gmail.com>
2019-06-24 16:43:02 -0400

    Update role.rb

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,7 +2,7 @@ class Role < ApplicationRecord
   #belongs_to :app
   #belongs_to :agent, class_name: "Agent", primary_key: :user_id
 
-  belongs_to :agent #, class_name: "Agent", foreign_key: "user_id"
+  belongs_to :agent, class_name: "Agent" #, foreign_key: "user_id"
   belongs_to :app
 
   scope :admin , ->{where("role =?", "admin")}
