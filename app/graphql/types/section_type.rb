ccefbfe4dc6b97efe4d75c55cfadcc9501ae1437
michelson <miguelmichelson@gmail.com>
2019-07-18 13:04:17 -0400

    article collection with sections

--- app/graphql/types/section_type.rb
+++ app/graphql/types/section_type.rb
@@ -3,5 +3,6 @@ module Types
     field :title, String, null: true
     field :id, Integer, null: true
     field :description, String, null: true
+    field :articles, [Types::ArticleType], null: false
   end
 end
