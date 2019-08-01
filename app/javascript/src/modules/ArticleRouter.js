20565a47468ffae2e4d26ad3625954a94feccae3
michelson <miguelmichelson@gmail.com>
2019-08-01 14:41:27 -0400

    centralize store

--- app/javascript/src/modules/ArticleRouter.js
+++ app/javascript/src/modules/ArticleRouter.js
@@ -3,10 +3,6 @@ import React, { Component } from 'react';
 import App from './App';
 
 import { Provider, connect } from 'react-redux'
-import { compose, createStore, applyMiddleware, combineReducers } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunkMiddleware from 'redux-thunk'
-import persistState from 'redux-localstorage'
 
 import Landing from '../pages/Landing'
 import graphql from '../graphql/client'
@@ -17,44 +13,10 @@ import { withStyles, createMuiTheme, MuiThemeProvider } from '@material-ui/core/
 import lightGreen from "@material-ui/core/colors/green";
 import blueGrey from "@material-ui/core/colors/blueGrey";
 
-import auth from '../actions/auth'
-import app from '../actions/app'
-import segment from '../actions/segments'
-import app_users from '../actions/app_users'
-import app_user from '../actions/app_user'
 
-import conversations from '../actions/conversations'
-import conversation from '../actions/conversation'
-import current_user from '../actions/current_user'
-import status_message from '../actions/status_messages'
-import current_page from '../actions/navigation'
 import Docs from '../pages/docs'
+import store from '../store'
 
-const rootReducer = combineReducers({
-  auth,
-  app,
-  segment,
-  app_users,
-  app_user,
-  conversation,
-  conversations,
-  current_user,
-  status_message,
-  current_page
-})
-
-const middlewares = [thunkMiddleware]//, routerMiddleware(history)]
-
-const enhancer = compose(
-  applyMiddleware(...middlewares),
-  persistState('auth', { key: 'AUTH' })
-)
-
-const store = createStore(rootReducer, composeWithDevTools(
-  enhancer
-));
-
-window.store = store
 
 
 class MainRouter extends Component {
---
+++ app/javascript/src/store.js
@@ -0,0 +1,42 @@
+import { compose, createStore, applyMiddleware, combineReducers } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk'
+import persistState from 'redux-localstorage'
+
+import auth from './actions/auth'
+import app from './actions/app'
+import segment from './actions/segments'
+import app_users from './actions/app_users'
+import app_user from './actions/app_user'
+
+import conversations from './actions/conversations'
+import conversation from './actions/conversation'
+import current_user from './actions/current_user'
+import status_message from './actions/status_messages'
+import current_page from './actions/navigation'
+
+const rootReducer = combineReducers({
+  auth,
+  app,
+  segment,
+  app_users,
+  app_user,
+  conversation,
+  conversations,
+  current_user,
+  status_message,
+  current_page
+})
+
+const middlewares = [thunkMiddleware]//, routerMiddleware(history)]
+
+const enhancer = compose(
+  applyMiddleware(...middlewares),
+  persistState('auth', { key: 'AUTH' })
+)
+
+const store = createStore(rootReducer, composeWithDevTools(
+  enhancer
+));
+
+export default store
