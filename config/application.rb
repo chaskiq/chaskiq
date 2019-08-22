e8113d17f6419f8d464d7d8d0ff2260e1ef7b3e9
michelson <miguelmichelson@gmail.com>
2019-08-22 12:04:38 -0400

    loads rails 6 defaults

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module Hermes
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    # config.load_defaults 5.2
+    config.load_defaults "6.0"
 
     config.action_cable.disable_request_forgery_protection = true
 
