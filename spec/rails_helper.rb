c8eb3d026572b78700a013ea840b7b945eb867a9
michelson <miguelmichelson@gmail.com>
2019-09-16 16:15:40 -0300

    specs on auth req, geo location stubs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,6 +43,8 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  Geocoder.configure(lookup: :test, ip_lookup: :test)
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
