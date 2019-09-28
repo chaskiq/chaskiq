17516200dc34e8f97baccb9fa7d99244fb8ef375
michelson <miguelmichelson@gmail.com>
2019-09-27 21:04:11 -0300

    agent bot default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_26_163125) do
+ActiveRecord::Schema.define(version: 2019_09_27_232347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2019_09_26_163125) do
     t.string "invited_by_type"
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
+    t.boolean "bot"
     t.index ["confirmation_token"], name: "index_agents_on_confirmation_token", unique: true
     t.index ["email"], name: "index_agents_on_email", unique: true
     t.index ["invitation_token"], name: "index_agents_on_invitation_token", unique: true
@@ -391,6 +392,7 @@ ActiveRecord::Schema.define(version: 2019_09_26_163125) do
     t.datetime "read_at"
     t.bigint "main_participant_id"
     t.boolean "priority"
+    t.string "email_message_id"
     t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -412,7 +414,7 @@ ActiveRecord::Schema.define(version: 2019_09_26_163125) do
     t.index ["eventable_type", "eventable_id"], name: "index_events_on_eventable_type_and_eventable_id"
   end
 
-  create_table "gutentag_taggings", id: :integer, default: nil, force: :cascade do |t|
+  create_table "gutentag_taggings", id: :serial, force: :cascade do |t|
     t.integer "tag_id", null: false
     t.integer "taggable_id", null: false
     t.string "taggable_type", null: false
@@ -423,7 +425,7 @@ ActiveRecord::Schema.define(version: 2019_09_26_163125) do
     t.index ["taggable_type", "taggable_id"], name: "index_gutentag_taggings_on_taggable_type_and_taggable_id"
   end
 
-  create_table "gutentag_tags", id: :integer, default: nil, force: :cascade do |t|
+  create_table "gutentag_tags", id: :serial, force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
