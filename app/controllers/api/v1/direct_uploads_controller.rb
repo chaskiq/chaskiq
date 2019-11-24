b86f43250f5485c8209c7437441f80251f211fc3
michelson <miguelmichelson@gmail.com>
2019-11-24 04:08:30 -0300

    Update direct_uploads_controller.rb
    use relative route

--- app/controllers/api/v1/direct_uploads_controller.rb
+++ app/controllers/api/v1/direct_uploads_controller.rb
@@ -12,7 +12,7 @@ private
 
   def direct_upload_json(blob)
     blob.as_json(root: false, methods: :signed_id)
-      .merge(service_url: url_for(blob))
+      .merge(service_url: rails_blob_path(blob))
       .merge(direct_upload:{
         url: blob.service_url_for_direct_upload,
         headers: blob.service_headers_for_direct_upload
