a63ae32a94e6efbccad56c923894f118c6ee1152
michelson <miguelmichelson@gmail.com>
2019-12-07 22:21:58 -0300

    chaskiq cable url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.enabled = false if ENV['ANYCABLE_DEPLOYMENT']
+
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
