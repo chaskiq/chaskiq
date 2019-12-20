77c26ab861adf61d8d490c25c5fcd55041f0a3c3
michelson <miguelmichelson@gmail.com>
2019-12-20 00:11:33 -0300

    specs , dynamic validations on app_package_integration

--- spec/models/app_package_integration_spec.rb
+++ spec/models/app_package_integration_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe AppPackageIntegration, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:app){
+    FactoryBot.create :app
+  }
+
+  it "create with validations" do
+    definitions =  [{
+      name: "api_secret",
+      type: 'string',
+      grid: { xs: 12, sm: 12 }
+    }]
+    package = AppPackage.create(name: "slack", definitions: definitions )
+    record = app.app_package_integrations.create(app_package: package)
+    expect(record.errors).to be_any
+  end
+
+  it "create without errors" do
+    definitions =  [{
+      name: "api_secret",
+      type: 'string',
+      grid: { xs: 12, sm: 12 }
+    }]
+    package = AppPackage.create(name: "slack", definitions: definitions )
+    record = app.app_package_integrations.create(app_package: package, api_secret: "12344")
+    expect(record.errors).to be_blank
+    expect(record).to be_persisted
+  end
+
+
 end
--- spec/models/app_package_spec.rb
+++ spec/models/app_package_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe AppPackage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "create" do
+    definitions =  [{
+      name: "api_secret",
+      type: 'string',
+      grid: { xs: 12, sm: 12 }
+    }]
+    package = AppPackage.create(name: "slack", definitions: definitions )
+
+    expect(package.name).to be_present
+    expect(package.definitions).to be_present
+    expect(package.definitions.size).to be == 1
+  end
+
 end
