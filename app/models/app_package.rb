bc2cef265d6675a7c7dd552a36090a05ed3921c9
michelson <miguelmichelson@gmail.com>
2019-12-20 00:11:33 -0300

    specs , dynamic validations on app_package_integration

--- app/models/app_package.rb
+++ app/models/app_package.rb
@@ -1,4 +1,9 @@
 class AppPackage < ApplicationRecord
   has_many :app_package_integrations
   has_many :apps, through: :app_package_integrations
+
+  store :settings, accessors: [ 
+    :definitions,
+  ], coder: JSON
+
 end
