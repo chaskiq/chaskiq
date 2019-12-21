17991943725d03905305b581035ed51cad7cff9f
michelson <miguelmichelson@gmail.com>
2019-12-21 03:09:25 -0300

    specs on DataEnrichment fullcontact
    + multiple providers supported

--- app/services/data_enrichment_service/full_contact.rb
+++ app/services/data_enrichment_service/full_contact.rb
@@ -33,5 +33,26 @@ module DataEnrichmentService
       JSON.parse(response.body, object_class: OpenStruct)
     end
 
+    def enrich_user(user)
+      response = self.get_data(params: {
+        email: user.email, 
+        macromeasures: true
+      })
+  
+      # means an error, escape it
+      return if response.status.present? && response.status >= 400
+
+      full_name = response.fullName
+      user.name = full_name
+      user.first_name = full_name.split(" ")[0]
+      user.last_name  = full_name.split(" ")[1]
+      user.twitter    = response.twitter
+      user.facebook   = response.facebook
+      user.linkedin   = response.linkedin
+      user.organization = response.organization
+      user.job_title = response.title
+      user.save
+    end
+
   end
 end
