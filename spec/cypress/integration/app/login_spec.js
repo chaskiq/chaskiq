154268658b032f8e8823a723d9f755d25678f2b2
michelson <miguelmichelson@gmail.com>
2019-12-23 11:34:44 -0300

    login & app creation spec

--- spec/cypress/integration/app/login_spec.js
+++ spec/cypress/integration/app/login_spec.js
@@ -1,4 +1,5 @@
 
+import {login} from '../../support/utils'
 
 describe('Login Spec', function() {
   beforeEach(() => {
@@ -6,33 +7,7 @@ describe('Login Spec', function() {
   })
 
   it('Sign in view', function() {
-    cy.appScenario('basic')
-
-    cy.visit('/')
-
-    cy.contains("Sign in")
-
-    cy.get('input[name="email"]')
-    .type('test@test.cl').should('have.value', 'test@test.cl')
-
-    cy.get('input[name="password"]')
-    .type('123456').should('have.value', '123456')
-
-    cy.get('button[type="submit"]').click()
-    cy.get("body").should('contain', 'Welcome to Chaskiq')
+    login()
   })
 
-
-
-  /*it('example of missing scenario failure', function() {
-    cy.visit('/')
-    cy.appScenario('basic')
-    // cy.appScenario('missing') // uncomment these if you want to see what happens
-  })
-
-  it('example of missing app failure', function() {
-    cy.visit('/')
-    cy.appScenario('basic')
-    // cy.app('run_me') // uncomment these if you want to see what happens
-  })*/
 })
