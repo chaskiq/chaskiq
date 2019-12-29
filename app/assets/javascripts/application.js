ad826ab0ad1fa035fc5f0b2e35554116a625458a
michelson <miguelmichelson@gmail.com>
2019-12-29 15:20:16 -0300

    I18n as pipeline asset

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,13 @@
 // require activestorage
 // require turbolinks
 //= require_tree .
+
+//
+// This is optional (in case you have `I18n is not defined` error)
+// If you want to put this line, you must put it BEFORE `i18n/translations`
+// require i18n
+// Some people even need to add the extension to make it work, see https://github.com/fnando/i18n-js/issues/283
+//= require i18n.js
+//
+// This is a must
+//= require i18n/translations
