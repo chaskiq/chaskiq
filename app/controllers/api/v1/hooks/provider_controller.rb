39dd444b17d09d3091bfa618f0bd341fff9f1ada
michelson <miguelmichelson@gmail.com>
2020-01-15 16:37:25 -0300

    twitter account activity

---
+++ app/controllers/api/v1/hooks/provider_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::Hooks::ProviderController < ApplicationController
+
+  before_action :find_application_package
+
+  def create
+    response = @integration_pkg.message_api_klass.create_hook_from_params(params)
+    render status: 200 , json: response.to_json
+  end
+
+  def process_event
+    @integration_pkg.message_api_klass.process_event(params)
+    render status: 200
+  end
+
+  def find_application_package
+    app = App.find_by(key: params[:app_key])
+    @integration_pkg = app.app_package_integrations
+                         .joins(:app_package)
+                         .where(
+                            id: params[:id], 
+                            "app_packages.name": params[:provider].capitalize
+                         ).first
+  end
+  
+end
--- app/models/app_package_integration.rb
+++ app/models/app_package_integration.rb
@@ -8,14 +8,28 @@ class AppPackageIntegration < ApplicationRecord
     api_key
     api_secret
     project_id
+    access_token
+    access_token_secret
   ], coder: JSON
 
   validate do
     app_package.definitions.each do |definition|
       key = definition[:name].to_sym
       next unless self.class.stored_attributes[:settings].include?(key)
-
       errors.add(key, "#{key} is required") if send(key).blank?
     end
   end
+
+
+  def message_api_klass
+    "MessageApis::#{app_package.name}".constantize.new(self.settings)
+  end
+
+  def handle_registration
+    klass = message_api_klass
+    url = CGI.escape("https://6404e5bc.ngrok.io/api/v1/hooks/#{app.key}/#{app_package.name.underscore}/#{self.id}")
+    klass.register_webhook(url)
+    klass.subscribe_to_events
+  end
+  
 end
