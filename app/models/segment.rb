ce5b6a8fbfdb96fe71293480a1b02d44f9a51b97
michelson <miguelmichelson@gmail.com>
2020-01-28 15:31:57 -0300

    segment comparator

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -180,9 +180,9 @@ class SegmentComparator
               when "not_eq" then user_input != predicate_value
               when "eq" then user_input == predicate_value
               when "lt" then user_input < predicate_value
-              when "lteq" then user_input <= predicate_value
-              when "gt" then user_input > predicate_value
-              when "gteq" then user_input >= predicate_value
+              when "lteq" then user_input.to_i <= predicate_value.to_i
+              when "gt" then user_input.to_i > predicate_value.to_i
+              when "gteq" then user_input.to_i >= predicate_value.to_i
               end    
   end
 
@@ -192,8 +192,10 @@ class SegmentComparator
         val = Chronic.parse(predicate['value'])
         check = compare_with(field, val, predicate['comparison'] )
         #check = cast_date(field).send(predicate['comparison'], Chronic.parse(predicate['value']))
-      when 'string', 'integer'
+      when 'string'
         check = compare_with(field, predicate['value'], predicate['comparison'] )
+      when 'integer'
+        check = compare_with(field.to_i, predicate['value'].to_i, predicate['comparison'] )
     end
     check
   end
