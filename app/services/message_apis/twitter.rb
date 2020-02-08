dd52407fa5fb45a5e341166f574edb6dae4d9cc0
michelson <miguelmichelson@gmail.com>
2020-02-08 02:55:49 -0300

    twitter specs , remove conversation_sources , implement conversation_channels

--- app/services/message_apis/twitter.rb
+++ app/services/message_apis/twitter.rb
@@ -47,11 +47,12 @@ module MessageApis
                   :twitter_api,
                   :base_url #Default: 'https://api.twitter.com/'
   
-  
     def initialize(config={})
       @base_url = 'https://api.twitter.com'
       @uploader_base_url = 'https://upload.twitter.com'
       @keys = {}
+
+      config = config[:config]
   
       @keys['consumer_key'] = config["api_key"]
       @keys['consumer_secret'] =  config["api_secret"]
@@ -59,31 +60,8 @@ module MessageApis
       @keys['access_token_secret'] =  config["access_token_secret"]
     end
 
-    def register_webhook(app_package, integration)
-      url = CGI.escape("#{ENV['HOST']}/api/v1/hooks/#{integration.app.key}/#{app_package.name.underscore}/#{integration.id}")
-      self.make_post_request("/1.1/account_activity/all/development/webhooks.json?url=#{url}", nil)
-    end
-
-    def get_webhooks
-      JSON.parse(self.make_get_request("/1.1/account_activity/all/development/webhooks.json"))
-    end
-
-    def delete_webhooks
-      get_webhooks.each do |o|
-        delete_webhook( o["id"] )
-      end
-    end
-
-    def delete_webhook(id)
-      self.make_delete_request("/1.1/account_activity/all/development/webhooks/#{id}.json")
-    end
-
-    def subscribe_to_events
-      self.make_post_request("/1.1/account_activity/all/development/subscriptions.json", nil)
-    end
-
-    #API client object is created with the @base_url context, then individual requests are made with specific URI paths passed in.
-  
+    #API client object is created with the @base_url context, 
+    # then individual requests are made with specific URI paths passed in.
     def get_api_access
       consumer = OAuth::Consumer.new(
         @keys['consumer_key'], 
@@ -105,82 +83,6 @@ module MessageApis
       @twitter_media_api = OAuth::AccessToken.from_hash(uploader_consumer, token)
     end
   
-    def make_post_request(uri_path, request)
-      get_api_access if @twitter_api.nil? #token timeout?
-
-      response = @twitter_api.post(uri_path, request, HEADERS)
-  
-      if response.code.to_i >= 300
-        puts "POST ERROR occurred with #{uri_path}, request: #{request} "
-        puts "Error code: #{response.code} #{response}"
-        puts "Error Message: #{response.body}"
-      end
-  
-      if response.body.nil? #Some successful API calls have nil response bodies, but have 2## response codes.
-         return response.code #Examples include 'set subscription', 'get subscription', and 'delete subscription'
-      else
-        return response.body
-      end
-  
-    end
-  
-    def make_get_request(uri_path)
-      get_api_access if @twitter_api.nil? #token timeout?
-  
-      response = @twitter_api.get(uri_path, HEADERS)
-      
-      if response.code.to_i >= 300
-        puts "GET ERROR occurred with #{uri_path}: "
-        puts "Error: #{response}"
-      end
-  
-      if response.body.nil? #Some successful API calls have nil response bodies, but have 2## response codes.
-        return response.code #Examples include 'set subscription', 'get subscription', and 'delete subscription'
-      else
-        return response.body
-      end
-    end
-  
-    def make_delete_request(uri_path)
-      get_api_access if @twitter_api.nil? #token timeout?
-  
-      response = @twitter_api.delete(uri_path, HEADERS)
-  
-      if response.code.to_i >= 300
-        puts "DELETE ERROR occurred with #{uri_path}: "
-        puts "Error: #{response}"
-      end
-  
-      if response.body.nil? #Some successful API calls have nil response bodies, but have 2## response codes.
-        return response.code #Examples include 'set subscription', 'get subscription', and 'delete subscription'
-      else
-        return response.body
-      end
-    end
-  
-    def make_put_request(uri_path)
-  
-      get_api_access if @twitter_api.nil? #token timeout?
-  
-      response = @twitter_api.put(uri_path, '', {"content-type" => "application/json"})
-  
-      if response.code.to_i == 429
-        puts "#{response.message}  - Rate limited..."
-      end
-  
-      if response.code.to_i >= 300
-        puts "PUT ERROR occurred with #{uri_path}, " #request: #{request} "
-        puts "Error: #{response}"
-      end
-  
-      if response.body.nil? #Some successful API calls have nil response bodies, but have 2## response codes.
-        return response.code #Examples include 'set subscription', 'get subscription', and 'delete subscription'
-      else
-        return response.body
-      end
-  
-    end
-
     def create_hook_from_params(params, package)
       crc_token = params['crc_token']
       if not crc_token.nil?
@@ -199,14 +101,43 @@ module MessageApis
       {status: :ok}
     end
 
+    def find_conversation_by_channel(channel)
+      conversation = @package
+      .app
+      .conversations
+      .joins(:conversation_channels)
+      .where(
+        "conversation_channels.provider =? AND 
+        conversation_channels.provider_channel_id =?", 
+        "twitter", channel
+      ).first
+    end
+
+    # incoming events from webhook
     def process_event(params, package)
+      @package = package
 
       app = package.app
 
       if params.keys.include?('direct_message_events')
         params['direct_message_events'].each do |message|
-          
+
           sender_id = message["message_create"]["sender_id"]
+          target_id = message["message_create"]["target"]["recipient_id"]
+          message_id = message["id"]
+          sender = params["users"][ sender_id ]
+          recipient = params["users"][ target_id ]
+          
+          # determine the id of the user (channel)
+          cond = sender_id == params['for_user_id']
+          channel_id = cond ? target_id : sender_id 
+          twitter_user = cond ? recipient : sender
+          agent_sender = cond
+          
+          conversation = find_conversation_by_channel(channel_id)
+
+          return if conversation && conversation.conversation_part_channel_sources
+          .find_by(message_source_id: message_id).present?
 
           message_data = message["message_create"]["message_data"]
           text = message_data["text"]
@@ -215,81 +146,70 @@ module MessageApis
             message_data
           )
           
-          sender = params["users"][ message["message_create"]["sender_id"] ]
-          
-          recipient = params["users"][ 
-            message["message_create"]["target"]["recipient_id"] 
-          ]
-          
-          agent_required = nil
+          participant = add_participant(twitter_user)
 
-          if sender_id == params['for_user_id'] 
-            agent_required = true
-            twitter_user = recipient
-          else
-            twitter_user = sender
-          end
-  
-          data = {
-            properties: {
-              name: sender["name"],
-              twitter_id: twitter_user["id"]
-            }
-          }
-          
-          participant = app.app_users.where(
-            "properties->>'twitter_id' = ?", twitter_user["id"]
-          ).first
+          #conversation = participant.conversations
+          #.joins(:conversation_source)
+          #.where("conversation_sources.app_package_integration_id =?", package.id)
+          #.first
 
-          ## todo: check user for this & previous conversation
-          ## via twitter with the twitter user id
-          participant = app.add_anonymous_user(data) if participant.blank?
-
-          conversation = participant.conversations
-          .joins(:conversation_source)
-          .where("conversation_sources.app_package_integration_id =?", package.id)
-          .first
+          conversation.conversation_channels.create({
+            provider: 'twitter',
+            provider_channel_id: channel_id
+          }) if conversation.present?
 
           conversation = app.conversations.create(
             main_participant: participant,
-            conversation_source_attributes:{
-              app_package_integration: package
-            } 
-          ) unless conversation.present?
+            conversation_channels_attributes: [
+              provider: 'twitter',
+              provider_channel_id: channel_id
+            ]
+          ) if conversation.blank?
 
+          # TODO: serialize message                 
           conversation.add_message(
-            from: agent_required ? Agent.first : participant,
+            from: agent_sender ? @package.app.agents.first : participant  , #agent_required ? Agent.first : participant,
             message: {
               html_content: text,
               serialized_content: serialized_content
             },
+            provider: 'twitter',
+            message_source_id: message_id,
             check_assignment_rules: true
           )
+
         end
       end
     end
 
-    def message_create_header(recipient_id)
+    # triggered when a new chaskiq message is created
+    # will triggered just after the ws notification
+    def notify_message(conversation: , part:, channel:)
+      # TODO ? redis cache here for provider / channel id / part
+      return if part.conversation_part_channel_sources.find_by(provider: "twitter").present?
+      
+      message = part.message.as_json
 
-      header = {}
-  
-      header['type'] = 'message_create'
-      header['message_create'] = {}
-      header['message_create']['target'] = {}
-      header['message_create']['target']['recipient_id'] = "#{recipient_id}"
-  
-      header
-  
+      response = send_message(conversation, message)
+
+      response_data = JSON.parse(response)
+
+      return unless response_data["event"]["id"].present?
+
+      part.conversation_part_channel_sources.create(
+        provider: 'twitter', 
+        message_source_id: response_data["event"]["id"]
+      )
     end
 
     def send_message(conversation, message)
       event = {}
       event['event'] = message_create_header(
         conversation.main_participant.properties['twitter_id']
-      )      
-
+      )   
+      
       blocks = JSON.parse(
-        message[:message][:serialized_content]
+        message["serialized_content"]
       )["blocks"]
 
       plain_message = blocks.map{|o| 
@@ -319,12 +239,46 @@ module MessageApis
       make_post_request("/1.1/direct_messages/events/new.json", event.to_json)
     end
 
+    def add_participant(twitter_user)
+      app = @package.app
+      if twitter_user
+        data = {
+          properties: {
+            name: twitter_user["name"],
+            twitter_id: twitter_user["id"]
+          }
+        }
+
+        participant = app.app_users.where(
+          "properties->>'twitter_id' = ?", twitter_user["id"]
+        ).first
+
+        ## todo: check user for this & previous conversation
+        ## via twitter with the twitter user id
+        participant = app.add_anonymous_user(data) if participant.blank?
+
+        participant
+      end
+    end
+
     def generate_crc_response(consumer_secret, crc_token)
       hash = OpenSSL::HMAC.digest('sha256', consumer_secret, crc_token)
       return Base64.encode64(hash).strip!
     end
 
     ## MESSAGE BLOCK METHODS CONVERSION
+
+    def message_create_header(recipient_id)
+      header = {}
+  
+      header['type'] = 'message_create'
+      header['message_create'] = {}
+      header['message_create']['target'] = {}
+      header['message_create']['target']['recipient_id'] = "#{recipient_id}"
+  
+      header
+  
+    end
 
     def serialize_content(data)
       data.keys.include?("attachment") ? 
@@ -467,7 +421,6 @@ module MessageApis
       ('a'..'z').to_a.shuffle[0,8].join
     end
 
-
     def make_post_media_request(uri_path, request, headers=nil)
       get_api_access if @twitter_media_api.nil? #token timeout?
 
@@ -550,6 +503,109 @@ module MessageApis
       )
     end
     # rubocop:enable MethodLength
+
+    # API Registration methods
+
+    def register_webhook(app_package, integration)
+      url = CGI.escape("#{ENV['HOST']}/api/v1/hooks/#{integration.app.key}/#{app_package.name.underscore}/#{integration.id}")
+      self.make_post_request("/1.1/account_activity/all/development/webhooks.json?url=#{url}", nil)
+    end
+
+    def get_webhooks
+      JSON.parse(self.make_get_request("/1.1/account_activity/all/development/webhooks.json"))
+    end
+
+    def delete_webhooks
+      get_webhooks.each do |o|
+        delete_webhook( o["id"] )
+      end
+    end
+
+    def delete_webhook(id)
+      self.make_delete_request("/1.1/account_activity/all/development/webhooks/#{id}.json")
+    end
+
+    def subscribe_to_events
+      self.make_post_request("/1.1/account_activity/all/development/subscriptions.json", nil)
+    end
+
+    # HTTPS methods
+
+    def make_post_request(uri_path, request)
+      get_api_access if @twitter_api.nil? #token timeout?
+
+      response = @twitter_api.post(uri_path, request, HEADERS)
+
+      if response.code.to_i >= 300
+        puts "POST ERROR occurred with #{uri_path}, request: #{request} "
+        puts "Error code: #{response.code} #{response}"
+        puts "Error Message: #{response.body}"
+      end
+  
+      if response.body.nil? #Some successful API calls have nil response bodies, but have 2## response codes.
+         return response.code #Examples include 'set subscription', 'get subscription', and 'delete subscription'
+      else
+        return response.body
+      end
+  
+    end
+  
+    def make_get_request(uri_path)
+      get_api_access if @twitter_api.nil? #token timeout?
+  
+      response = @twitter_api.get(uri_path, HEADERS)
+      
+      if response.code.to_i >= 300
+        puts "GET ERROR occurred with #{uri_path}: "
+        puts "Error: #{response}"
+      end
+  
+      if response.body.nil? #Some successful API calls have nil response bodies, but have 2## response codes.
+        return response.code #Examples include 'set subscription', 'get subscription', and 'delete subscription'
+      else
+        return response.body
+      end
+    end
+  
+    def make_delete_request(uri_path)
+      get_api_access if @twitter_api.nil? #token timeout?
+  
+      response = @twitter_api.delete(uri_path, HEADERS)
+  
+      if response.code.to_i >= 300
+        puts "DELETE ERROR occurred with #{uri_path}: "
+        puts "Error: #{response}"
+      end
+  
+      if response.body.nil? #Some successful API calls have nil response bodies, but have 2## response codes.
+        return response.code #Examples include 'set subscription', 'get subscription', and 'delete subscription'
+      else
+        return response.body
+      end
+    end
+  
+    def make_put_request(uri_path)
+  
+      get_api_access if @twitter_api.nil? #token timeout?
+  
+      response = @twitter_api.put(uri_path, '', {"content-type" => "application/json"})
+  
+      if response.code.to_i == 429
+        puts "#{response.message}  - Rate limited..."
+      end
+  
+      if response.code.to_i >= 300
+        puts "PUT ERROR occurred with #{uri_path}, " #request: #{request} "
+        puts "Error: #{response}"
+      end
+  
+      if response.body.nil? #Some successful API calls have nil response bodies, but have 2## response codes.
+        return response.code #Examples include 'set subscription', 'get subscription', and 'delete subscription'
+      else
+        return response.body
+      end
+  
+    end
 
   end
 
