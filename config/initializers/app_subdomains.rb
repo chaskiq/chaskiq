b2ea8a80e0048e81f47577a6b6f30530b8d7d9db
michelson <miguelmichelson@gmail.com>
2020-03-06 09:20:27 -0300

    Update app_subdomains.rb

--- config/initializers/app_subdomains.rb
+++ config/initializers/app_subdomains.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-url = Addressable::URI.parse(ENV['HOST'])
-subdomain = url.host.gsub(url.domain, "").gsub(".", "")
+url = Addressable::URI.parse(ENV['HOST']) 
+subdomain = url.host.gsub(url.domain, "").gsub(".", "") rescue nil
 
 APP_SUBDOMAINS = ["www", "app", subdomain].compact.uniq.freeze
