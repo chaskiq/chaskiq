ce998a17742ab64d8a4d44d80a25b03186a1e80f
michelson <miguelmichelson@gmail.com>
2020-04-18 04:10:35 -0400

    remove old src

--- app/javascript/old-src/editor/components/blocks/commons.js
+++
@@ -1,9 +0,0 @@
-import { updateDataOfBlock } from '../../model/index.js'
-
-export const UpdateData = (t)=> {
-  let { blockProps, block } = t.props
-  let { getEditorState, setEditorState } = t.props.blockProps
-  let data = block.getData()
-  let newData = data.merge(t.state).merge({ forceUpload: false })
-  return setEditorState(updateDataOfBlock(getEditorState(), block, newData))
-}
--- app/javascript/old-src/images/readme.txt
+++
@@ -1,8 +0,0 @@
-
-
-========================================================
- This pattern is downloaded from www.subtlepatterns.com 
- If you need more, that's where to get'em.
- ========================================================
- 
- 
