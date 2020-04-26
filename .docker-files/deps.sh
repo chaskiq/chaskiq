802e7f46e909a2982e3f73ce40544f0bbeca2169
Luis Mancilla Ávila <luis.mancilla.avila@gmail.com>
2020-04-26 17:34:31 -0400

    Dockerfile stages. Production purposes

---
+++ .docker-files/deps.sh
@@ -0,0 +1,11 @@
+set -x
+
+# Install Dependencies
+apt-get update -qq \
+  && DEBIAN_FRONTEND=noninteractive apt-get install -yq --no-install-recommends \
+    build-essential \
+    gnupg2 \
+    curl \
+    less \
+    git \
+    vim
