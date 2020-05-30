8e82c9ccb61a83ba5c7f067c905c1181ff7fa568
michelson <miguelmichelson@gmail.com>
2020-05-29 22:46:12 -0400

    password recovery - ssr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
 import React from 'react'
 import ReactDOM from 'react-dom'
 
