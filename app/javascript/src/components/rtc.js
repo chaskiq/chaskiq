2d04112cd2287ac5e6e7c4e9c8873b0c3560998e
michelson <miguelmichelson@gmail.com>
2020-06-01 23:39:12 -0400

    Update rtc.js

--- app/javascript/src/components/rtc.js
+++ app/javascript/src/components/rtc.js
@@ -168,39 +168,6 @@ export function RtcView (props) {
         audio: true,
         video: true
       }
-
-
-      /*navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true
-      }).then(gotMedia).catch(() => {})
-      
-      function gotMedia (stream) {
-        var peer1 = new Peer({ initiator: true, stream: stream })
-        var peer2 = new Peer()
-      
-        peer1.on('signal', data => {
-          peer2.signal(data)
-        })
-      
-        peer2.on('signal', data => {
-          peer1.signal(data)
-        })
-      
-        peer2.on('stream', stream => {
-          // got remote video stream, now let's show it in a video tag
-          var video = document.querySelector('video')
-      
-          if ('srcObject' in video) {
-            video.srcObject = stream
-          } else {
-            video.src = window.URL.createObjectURL(stream) // for older browsers
-          }
-      
-          video.play()
-        })
-      }*/
-
       
       navigator.mediaDevices.getUserMedia(op).then(gotMedia).catch((err) => {
         console.log("oeoeoe", err)
