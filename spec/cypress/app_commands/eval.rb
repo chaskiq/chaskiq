be8c3a0123b36c46847120b2fbda303e5db80d61
michelson <miguelmichelson@gmail.com>
2020-06-15 23:18:12 -0400

    use AR pool for cy cleaning

--- spec/cypress/app_commands/eval.rb
+++ spec/cypress/app_commands/eval.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
-Kernel.eval(command_options) unless command_options.nil?
+ActiveRecord::Base.connection_pool.with_connection do
+  if Rails.env.test?
+    Kernel.eval(command_options) unless command_options.nil?
+  end
+end
