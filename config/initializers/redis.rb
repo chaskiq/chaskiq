cccb0bff00210668d44a9ee0e9c6f1c06ec1c2c7
michelson <miguelmichelson@gmail.com>
2020-07-02 22:33:57 -0400

    Update redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -5,6 +5,15 @@
 # your existing connection. However, you need to make sure that both
 # :timeout and :size are set appropriately in a multithreaded environment.
 
+
+# `Redis#exists(key)` will return an Integer in redis-rb 4.3. `exists?` 
+# returns a boolean, you should use it instead. To opt-in to the new 
+# behavior now you can set Redis.exists_returns_integer =  true. 
+# To disable this message and keep the current (boolean) behaviour of 'exists' 
+# you can set `Redis.exists_returns_integer = false`, 
+# but this option will be removed in 5.0. (gems/redis-namespace-1.6.0/lib/redis/namespace.rb:442:in `call_with_namespace')
+Redis.exists_returns_integer =  true
+
 require 'connection_pool'
 Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) do
   Redis.new(url: ENV.fetch('REDIS_URL') { 'redis://localhost:6379/1' })
