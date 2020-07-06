d9cde9d47613c29e5fd7501901691ae7952b3724
michelson <miguelmichelson@gmail.com>
2020-07-05 20:39:52 -0400

    quick replies i18n

--- app/javascript/src/components/Button.js
+++ app/javascript/src/components/Button.js
@@ -120,10 +120,10 @@ const SizeButton = styled(BaseButton)`
   }};
 `
 
-export default function Button ({ children, ...buttonProps }) {
+export default function Button ({ children, className, ...buttonProps }) {
   return (
     <SizeButton
-      className={'transition duration-150 ease-in-out'}
+      className={`transition duration-150 ease-in-out ${className || ''}`}
       // className="px-2 py-1 rounded-lg bg-green-400 text-green-800 text-xl font-light uppercase shadow-md hover:shadow-lg"
       {...buttonProps}
     >
