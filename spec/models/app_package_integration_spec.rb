39584811599feed09f0a813a89ae23d494e7e35a
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2020-10-08 03:32:29 -0300

    more packages specs

--- spec/models/app_package_integration_spec.rb
+++ spec/models/app_package_integration_spec.rb
@@ -7,32 +7,18 @@ RSpec.describe AppPackageIntegration, type: :model do
   before :each do 
     AppPackagesCatalog.update_all
   end
-  
+
   let(:app) do
     FactoryBot.create :app
   end
 
   it 'create with validations' do
-    definitions = [{
-      name: 'api_secret',
-      type: 'string',
-      grid: { xs: 12, sm: 12 }
-    }]
-    package = AppPackage.create(name: 'slack', definitions: definitions)
-    record = app.app_package_integrations.create(app_package: package)
+    package = AppPackage.find_by(name: 'Twitter')
+    record = app.app_package_integrations.create(
+      app_package: package
+    )
     expect(record.errors).to be_any
   end
-
-  it 'create without errors' do
-    package = AppPackage.find_by(name: 'Slack')
-    record = app.app_package_integrations.create(
-      app_package: package, 
-      api_secret: '12344'
-    )
-    expect(record.errors).to be_blank
-    expect(record).to be_persisted
-  end
-
 
   it "handle registrations" do
     package = AppPackage.find_by(name: 'Twitter')
