65203dd09b64c219bcb557ced2127bafa7bba158
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2020-10-15 01:58:55 +0000

    DEFAULT_SENDER_EMAIL

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = ENV['SES_ADDRESS']
+  config.mailer_sender = ENV['DEFAULT_SENDER_EMAIL']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
