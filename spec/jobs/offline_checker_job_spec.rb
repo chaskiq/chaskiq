02f8a7d942bdac47b302f0c28da3347cb2aa5847
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2021-02-12 00:41:58 -0300

    spec file

--- spec/jobs/offline_checker_job_spec.rb
+++ spec/jobs/offline_checker_job_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe OfflineCheckerJobJob, type: :job do
+RSpec.describe OfflineCheckerJob, type: :job do
   pending "add some examples to (or delete) #{__FILE__}"
 end
