a539061f1cdf850f14b00fe06adc33db7c6d54be
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2021-02-22 01:11:32 -0300

    refactors on campaigns model, move bot tasks to campaing model

--- spec/cypress/integration/messenger/task_bot_spec.js
+++ spec/cypress/integration/messenger/task_bot_spec.js
@@ -1,6 +1,6 @@
 import helpers from './helpers.js'
 
-function addPackage(app_package){
+function addPackage (app_package) {
   cy.appEval(`
     app = App.first
     app_package = AppPackage.find_by(name: "${app_package}")
@@ -11,7 +11,6 @@ function addPackage(app_package){
 }
 
 describe('Task bot Spec', function () {
-
   beforeEach(() => {
     cy.appEval('ActiveJob::Base.queue_adapter = :test')
     cy.appEval('ActiveJob::Base.queue_adapter.perform_enqueued_at_jobs = true')
@@ -21,7 +20,6 @@ describe('Task bot Spec', function () {
       AppPackagesCatalog.update_all 
     `)
   })
-
 
   it('sessionless never ask email', function () {
     cy.appScenario('app_bot_settings', { email_requirement: 'never' }).then(() => {
@@ -63,9 +61,8 @@ describe('Task bot Spec', function () {
   it('sessionless 2 always ask email , email validation', function () {
     cy.wait(5000)
 
-
     cy.appScenario('app_bot_settings', { email_requirement: 'Always' }).then((results) => {
-      addPackage("Qualifier")
+      addPackage('Qualifier')
 
       cy.appEval('App.last').then((results) => {
         const appKey = results.key
@@ -96,7 +93,7 @@ describe('Task bot Spec', function () {
                           cy.wrap($body).contains('will reply as soon as they can.')
 
                           cy.wrap($body).contains('Are you an existing')
-                          
+
                           cy.wrap($body).contains("Yes, I'm a customer").click().then(() => {
                             cy.wrap($body).contains('by email')
                             cy.wrap($body)
@@ -110,8 +107,8 @@ describe('Task bot Spec', function () {
                             cy.wrap($body).contains('is invalid')
 
                             cy.wrap($body)
-                            .xpath('/html/body/div/div/div/div[2]/div[1]/div/div/div[1]/div[1]/div/form/div/div/div/input')
-                            .type('John@apple.test')
+                              .xpath('/html/body/div/div/div/div[2]/div[1]/div/div/div[1]/div[1]/div/form/div/div/div/input')
+                              .type('John@apple.test')
 
                             cy.wrap($body)
                               .xpath('/html/body/div/div/div/div[2]/div[1]/div/div/div[1]/div[1]/div/form/div/div/div/div')
@@ -127,7 +124,6 @@ describe('Task bot Spec', function () {
       })
     })
   })
-
 
   it('sessionless 3 never ask email', function () {
     cy.appScenario('app_bot_settings', { email_requirement: 'never' }).then(() => {
@@ -173,10 +169,10 @@ describe('Task bot Spec', function () {
     })
   })
 
-  it('sessionless bot task wait for reply', function () {
+  it.only('sessionless bot task wait for reply', function () {
     cy.app('clean')
     cy.wait(6000)
-    cy.appScenario('basic').then((basic)=>{
+    cy.appScenario('basic').then((basic) => {
       cy.log('basic', JSON.stringify(basic))
 
       cy.appEval('App.last').then((results) => {
@@ -187,26 +183,23 @@ describe('Task bot Spec', function () {
           app_key: appKey
         }).then((res) => {
           cy.log('bot task command', JSON.stringify(res))
-  
+
           cy.visit(`/tester/${appKey}?sessionless=true`).then(() => {
             cy.get('[data-chaskiq-container] iframe')
               .then(function ($iframe) {
                 const $body = $iframe.contents().find('body')
-  
+
                 cy.wrap($body).contains('one')
                 cy.wrap($body).contains('two')
-                cy.wrap($body).contains('tree').then(()=>{
+                cy.wrap($body).contains('tree').then(() => {
                   cy.wait(1000)
 
                   cy.wrap($body)
-                  .xpath('/html/body/div/div/div/div[2]/div/div/div/div[2]/div/div/textarea')
-                  .type('oeoe \n').then(()=> {
-                    cy.wrap($body).contains('four')
-                  })
-
+                    .xpath('/html/body/div/div/div/div[2]/div/div/div/div[2]/div/div/textarea')
+                    .type('oeoe \n').then(() => {
+                      cy.wrap($body).contains('four')
+                    })
                 })
-  
-                
               })
           })
         })
@@ -215,14 +208,13 @@ describe('Task bot Spec', function () {
   })
 
   describe('Task bot Spec', function () {
-
-    beforeEach(()=>{
+    beforeEach(() => {
       cy.app('clean')
       cy.wait(6000)
     })
-    
+
     it('sessionless enter bot on new conversation with empty predicates', function () {
-      cy.appScenario('basic').then(()=>{
+      cy.appScenario('basic').then(() => {
         cy.appEval('App.last').then((results) => {
           const appKey = results.key
           cy.log('App.last', JSON.stringify(results))
@@ -241,10 +233,9 @@ describe('Task bot Spec', function () {
               app_key: appKey
             }).then((res) => {
               cy.appEval(`BotTask.find(${res.id}).update(state: 'disabled' )`)
-      
+
               cy.log('task command', JSON.stringify(res))
 
-      
               helpers.openMessenger('?sessionless=true&lang=en', ($body, appKey) => {
                 expect($body.html()).to.contain('Start a conversation')
                 cy.wrap($body)
@@ -272,7 +263,7 @@ describe('Task bot Spec', function () {
     })
 
     it('sessionless enter bot on new conversation with predicates', function () {
-      cy.appScenario('basic').then(()=>{
+      cy.appScenario('basic').then(() => {
         cy.appEval('App.last').then((results) => {
           const appKey = results.key
           cy.appEval(`App.find_by(key: '${appKey}').update(lead_tasks_settings: {
@@ -288,14 +279,14 @@ describe('Task bot Spec', function () {
                 }]
               }
             ]
-          })`).then(()=>{
+          })`).then(() => {
             cy.app('bot_task_command', {
               app_key: appKey
             }).then((res) => {
               cy.appEval(`BotTask.find(${res.id}).update(state: 'disabled' )`)
-      
+
               cy.log(res)
-      
+
               helpers.openMessenger('?sessionless=true&lang=en', ($body, appKey) => {
                 expect($body.html()).to.contain('Start a conversation')
                 cy.wrap($body)
@@ -318,22 +309,21 @@ describe('Task bot Spec', function () {
               })
             })
           })
-    
         })
       })
     })
 
     it('sessionless default bot new conversation', function () {
-      cy.appScenario('basic').then(()=>{
+      cy.appScenario('basic').then(() => {
         cy.appEval('App.last').then((results) => {
-          const appKey = results.key  
+          const appKey = results.key
           cy.app('bot_task_command', {
             app_key: appKey
           }).then((res) => {
             cy.appEval(`BotTask.find(${res.id}).update(state: 'disabled' )`)
-    
+
             cy.log(res)
-    
+
             helpers.openMessenger('?sessionless=true&lang=en', ($body, appKey) => {
               expect($body.html()).to.contain('Start a conversation')
               cy.wrap($body)
@@ -358,7 +348,7 @@ describe('Task bot Spec', function () {
     })
 
     it('user session default bot new conversation', function () {
-      cy.appScenario('basic').then(()=>{
+      cy.appScenario('basic').then(() => {
         cy.appEval('App.last').then((results) => {
           const appKey = results.key
           cy.appEval(`App.find_by(key: '${appKey}').update(user_tasks_settings: {
@@ -379,9 +369,9 @@ describe('Task bot Spec', function () {
               app_key: appKey
             }).then((res) => {
               cy.appEval(`BotTask.find(${res.id}).update(state: 'disabled' )`)
-      
+
               cy.log(res)
-      
+
               helpers.openMessenger('', ($body, appKey) => {
                 expect($body.html()).to.contain('Start a conversation')
                 cy.wrap($body)
@@ -395,7 +385,7 @@ describe('Task bot Spec', function () {
                           .xpath('/html/body/div/div/div/div[2]/div/div/div/div[2]/div/div/textarea')
                           .type('oeoe \n').then(() => {
                             cy.wrap($body).contains('oeoe')
-                            
+
                             cy.wrap($body).contains('one')
                             cy.wrap($body).contains('two')
                             cy.wrap($body).contains('tree')
@@ -410,7 +400,7 @@ describe('Task bot Spec', function () {
     })
 
     it('user session default bot new conversation', function () {
-      cy.appScenario('basic').then(()=>{
+      cy.appScenario('basic').then(() => {
         cy.appEval('App.last').then((results) => {
           const appKey = results.key
           cy.appEval(`App.find_by(key: '${appKey}').update(user_tasks_settings: {
@@ -437,9 +427,9 @@ describe('Task bot Spec', function () {
               app_key: appKey
             }).then((res) => {
               cy.appEval(`BotTask.find(${res.id}).update(state: 'disabled' )`)
-      
+
               cy.log(res)
-      
+
               helpers.openMessenger('', ($body, appKey) => {
                 expect($body.html()).to.contain('Start a conversation')
                 cy.wrap($body)
@@ -453,7 +443,7 @@ describe('Task bot Spec', function () {
                           .xpath('/html/body/div/div/div/div[2]/div/div/div/div[2]/div/div/textarea')
                           .type('oeoe \n').then(() => {
                             cy.wrap($body).contains('oeoe')
-                            
+
                             cy.wrap($body).contains('one')
                             cy.wrap($body).contains('two')
                             cy.wrap($body).contains('tree')
@@ -465,7 +455,6 @@ describe('Task bot Spec', function () {
           })
         })
       })
-
     })
   })
 })
