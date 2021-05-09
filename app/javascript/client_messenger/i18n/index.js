d26c5ea4b8c0122f7bda72445eba96ede0aec0ad
Guillaume BAILLEUL <guillaume.bailleul@gmail.com>
2021-05-09 17:06:43 +0200

    Add french translation for client
    Hello, 
    
    here is a proposal for chat client french translation. I need it for my current projet. 
    I am french speaking so, will it is probably not probably not perfect it is at least written in a decent language.
    
    Best regards

--- app/javascript/client_messenger/i18n/index.js
+++ app/javascript/client_messenger/i18n/index.js
@@ -126,7 +126,67 @@ i18n
             <p> ¿Te parece bien? </p>
           `
         }
-      }
+      },
+      fr: {
+        translations: {
+          runon: 'Nous utilisons Chaskiq',
+          conversations: 'Conversations',
+          'conversation_block.choosen': 'Vous avez répondu <strong>{{field}}</strong>',
+          continue_conversation: 'Continuer la conversation',
+          start_conversation: 'Commencer une conversation',
+          view_all_conversations: 'voir toutes les conversations',
+          see_previous: 'Voir les précedentes',
+          search_article_title: "Trouver les réponses dans notre répertoire d'article",
+          search_articles: 'Rechercher un article',
+          latest_articles: 'Nos derniers articles',
+          reply_above: 'Répondre en dessous',
+          create_new_conversation: 'créer une nouvelle conversation',
+          you: 'Vous',
+          not_seen: 'non lu',
+          by: 'Par',
+          dismiss: 'Rejeter',
+          submit: 'Soumettre',
+          article_meta: 'Par: <strong>{{name}}</strong> le <0>{{date}}</0>',
+          enter_your: 'Entrez votre {{field}}',
+
+          'editor.placeholder': 'Envoyer un message...',
+          'conversations.events.assigned': '{{name}} a été assigné à cette conversation',
+          'conversations.message_blocks.ask_option': 'attend votre réponse',
+          'conversations.message_blocks.data_retrieval': 'attend votre réponse',
+          'conversations.message_blocks.app_package_wait_reply': 'attend votre réponse',
+          'conversations.message_blocks.app_package_non_wait': 'vous a envoyé un contenu',
+
+          'reply_time.auto': 'L\'équipe va vous répondre dès que possible',
+          'reply_time.minutes': 'L\'équipe répond habituellement en quelques minutes',
+          'reply_time.hours': 'L\'équipe répond habituellement en quelques heures',
+          'reply_time.1 day': 'L\'équipe répond habituellement en quelques jours',
+
+          is_typing: '{{name}} est en train d\'écrire',
+          'availability.aprox': 'nous serons de retour dans quelques {{time}} heures',
+          'availability.tomorrow': "nous serons de retour dans demain",
+          'availability.days': "nous serons de retour dans quelque {{val}} jours",
+          'availability.next_week': "nous serons de retour la semaine prochaine",
+          'availability.back_from': "nous serons en ligne à partir de {{hours}} heure",
+          'availability.tomorrow_from': "nous serons en ligne demain à partir de {{hours}} heure",
+          'tours.done': 'Fait !',
+          invalid: '{{name}} entré est incorrect',
+          gdpr_title: 'Protection des données',
+          gdpr_ok: 'Oui, accepter',
+          gdpr_nok: 'Non, pas maintenant',
+          gdpr: `
+            <p>
+              Bonjour ! Chez {{name}} nous aimons vous parler.
+              Sous la GRPD (General Data Protection Regulation), nous avons besoins de votre approbation pour utiliser les informations personnelles
+              (ex: votre nom et adresse email) que vous nous fournissez quand nous communiquons:
+            </p>
+            <p>(1) Nous allons stocker vos informations personnelles pour que nous puissions sélectionner une converser plus tard.</p>
+            <p>(2) Nous avons besoin d'un email pour suivre nos conversations.</p>
+            <p>(3) Nous pourrions envoyer des email a propos des futurs services ou offres de {{name}}.</p>
+
+            <p>êtes vous d'accord ?</p>
+          `
+        }
+      },
     },
     fallbackLng: 'en',
     debug: false,
