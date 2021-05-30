b1cff5c626b114d660220520f54237a47d3461d0
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2021-05-30 17:57:59 -0400

    provisory revert

--- app/models/external_profile.rb
+++ app/models/external_profile.rb
@@ -5,7 +5,7 @@ class ExternalProfile < ApplicationRecord
   def app_package
     app.app_package_integrations
        .joins(:app_package)
-       .find_by("app_packages.name": provider.capitalize)
+       .where("app_packages.name": provider.capitalize)
        .first
   end
 
