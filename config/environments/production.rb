e2ced8faddf82cab81c38a1183d1f8b3a5063097
Kips <patrickjdavenport@gmail.com>
2021-06-22 20:20:58 +0100

    use SES region from envar
    use SES region from envar

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,6 +118,8 @@ Rails.application.configure do
     ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
                                            access_key_id: ENV['AWS_ACCESS_KEY_ID'],
                                            secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+                                           server: "email.#{ENV['SES_REGION']}.amazonaws.com",
+                                           message_id_domain: "#{ENV['SES_REGION']}.amazonses.com",
                                            signature_version: 4
 
     config.action_mailer.delivery_method = :ses
