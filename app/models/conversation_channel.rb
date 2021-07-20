1670a612ed43a9014cf633ef4f578922faa02cad
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2021-07-19 22:33:50 -0400

    empty package skip notification

--- app/models/conversation_channel.rb
+++ app/models/conversation_channel.rb
@@ -8,6 +8,12 @@ class ConversationChannel < ApplicationRecord
                       .where("app_packages.name =?", provider.classify)
                       .limit(1).first
 
+                     
+    # TODO: notify from time to time that this package is not delivering
+    # due to there is no installed package,
+    # this could happen when channels are available but there is no package added
+    return if pkg.blank? 
+
     pkg.message_api_klass.notify_message(
       conversation: conversation,
       part: part,
