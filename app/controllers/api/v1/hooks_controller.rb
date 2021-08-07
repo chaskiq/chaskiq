303b230a222955b1e00bd65addc058c7cf926f17
Miguel michelson <miguelmichelson@gmail.com>
2021-08-06 23:06:18 -0400

    force encoding on body email message

--- app/controllers/api/v1/hooks_controller.rb
+++ app/controllers/api/v1/hooks_controller.rb
@@ -64,7 +64,8 @@ class Api::V1::HooksController < ActionController::API
 
     # EmailReplyParser.parse_reply(mail.text_part.body.to_s)
     # message = EmailReplyParser.parse_reply(mail.text_part.body.to_s).gsub("\n", "<br/>").force_encoding(Encoding::UTF_8)
-    message = EmailReplyTrimmer.trim(mail.text_part.body.to_s).gsub("\n", "<br/>").force_encoding(Encoding::UTF_8)
+    body = mail.text_part.body.to_s.force_encoding(Encoding::UTF_8)
+    message = EmailReplyTrimmer.trim(body).gsub("\n", "<br/>")
 
     app, conversation, from = handle_conversation_part(mail)
 
