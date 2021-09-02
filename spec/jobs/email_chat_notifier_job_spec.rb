70f0d95a47243fdfc34c23aab43311557461783e
Miguel Michelson Martinez <miguel@chaskiq.io>
2021-09-02 15:14:51 -0400

    Update spec/jobs/email_chat_notifier_job_spec.rb
    Co-authored-by: Olivier <olimart@users.noreply.github.com>

--- spec/jobs/email_chat_notifier_job_spec.rb
+++ spec/jobs/email_chat_notifier_job_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe EmailChatNotifierJob, type: :job do
-  it "enqueue job" do
+  it "enqueues job" do
     ActiveJob::Base.queue_adapter = :test
     expect {
       EmailChatNotifierJob.perform_later({})
