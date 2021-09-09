34a64d58e6374d28d57fcee2d5c5c4501e3a5033
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2021-09-09 03:16:45 -0300

    revert scroll hide on homePanel

--- app/javascript/packages/messenger/src/client_messenger/homePanel.tsx
+++ app/javascript/packages/messenger/src/client_messenger/homePanel.tsx
@@ -378,14 +378,6 @@ const Panel = styled.div`
   width: 100%;
   height: 94%;
   z-index: 1000;
-  /* Hide scrollbar for Chrome, Safari and Opera */
-  &::-webkit-scrollbar {
-    display: none;
-  }
-
-  /* Hide scrollbar for IE, Edge and Firefox */
-  -ms-overflow-style: none; /* IE and Edge */
-  scrollbar-width: none; /* Firefox */
 `;
 
 const Availability = styled.div`
