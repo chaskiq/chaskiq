cc1c0a4b698e185b7a7d5b9fb562ad9984cdfa93
Heitor Carvalho <heitor.scz@gmail.com>
2021-09-19 14:44:33 -0300

    Parametrize active storage service config option
    The default storage is still Amazon S3 so current implementations do not break. The new environment variable 'STORAGE_SERVICE'  can be used to choose other storage services specified on storage.yml.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
 
-  config.active_storage.service = :amazon
+  config.active_storage.service = ENV['STORAGE_SERVICE'].try(:to_sym) || :amazon
 
   Rails.application.routes.default_url_options = { host: ENV['HOST'] }
   config.action_controller.default_url_options = { host: ENV['HOST'] }
