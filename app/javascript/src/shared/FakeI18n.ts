8d1f8d1e6cf345e4d833ac11c18e33cc8df7b084
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2021-09-26 17:25:30 -0300

    enable i18n fallback

--- app/javascript/src/shared/FakeI18n.ts
+++ app/javascript/src/shared/FakeI18n.ts
@@ -1,6 +1,6 @@
 import { I18n } from 'i18n-js';
 import translations from '../locales/translations.json'; // '../src/locales/translations.json';
 const i18n = new I18n(translations);
-i18n.enableFallback = true
+i18n.enableFallback = true;
 i18n.locale = document.documentElement.lang || 'en';
 export default i18n;
