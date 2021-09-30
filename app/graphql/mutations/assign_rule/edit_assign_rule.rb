057289bb2caa26c7e4a5091c4faa86040b3637c8
Nuxor3000 <72187088+Nuxor3000@users.noreply.github.com>
2021-09-30 12:04:34 +0200

    Update edit_assign_rule.rb

--- app/graphql/mutations/assign_rule/edit_assign_rule.rb
+++ app/graphql/mutations/assign_rule/edit_assign_rule.rb
@@ -9,11 +9,10 @@ module Mutations
       argument :app_key, String, required: true
       argument :agent_id, String, required: true
       argument :title, String, required: true
-      argument :active, String, required: true
       argument :rule_id, Integer, required: true
       argument :conditions, Types::JsonType, required: true
 
-      def resolve(app_key:, agent_id:, title:, active:, rule_id:, conditions:)
+      def resolve(app_key:, agent_id:, title:, rule_id:, conditions:)
         find_app(app_key)
         @agent = @app.agents.find(agent_id)
         assignment_rule = @app.assignment_rules.find(rule_id)
