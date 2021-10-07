854a805aaf8fd397f5017a62f23301d7907ba453
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2021-10-07 01:47:44 -0300

    api client connection

--- app/services/message_apis/bootic/api.rb
+++ app/services/message_apis/bootic/api.rb
@@ -6,7 +6,9 @@ module MessageApis::Bootic
   class Api < MessageApis::BasePackage
     include MessageApis::Helpers
 
-    attr_accessor :url, :access_token, :conn
+    attr_accessor :access_token, :conn
+
+    URL = "https://api.bootic.net/v1"
 
     def initialize(config:)
       @package = config[:package]
@@ -20,10 +22,45 @@ module MessageApis::Bootic
       @conn.headers = {
         "Content-Type" => "application/json",
         "Accept" => "application/json",
-        "Authorization" => "Bearer #{@access_token}"
+        "Authorization" => "Bearer #{@package.access_token}"
       }
 
       self
+    end
+
+    def url(path)
+      "#{URL}/#{path}"
+    end
+
+    def test
+      @conn.get(URL)
+    end
+
+    # https://api.bootic.net/rels/products/
+    def get_products
+      @conn.get(url("products.json"), { shop_ids: "4138" })
+    end
+
+    def get_product(id)
+      @conn.get(url("products/#{id}.json"))
+    end
+
+    def get_contacts
+      @conn.get(url("contacts.json"))
+    end
+
+    def create_webhook(attrs = {
+      topic: "orders.updated.placed",
+      hub_id: 1
+    })
+      hub_id = attrs[:hub_id]
+      attributes = {
+        topic: attrs[:topic],
+        url: @package.hook_url,
+        notify_origin: false
+        # "auth": {}
+      }
+      @conn.post(url("/shops/#{hub_id}/hub"), attributes)
     end
 
     def register_webhook(app_package, integration); end
