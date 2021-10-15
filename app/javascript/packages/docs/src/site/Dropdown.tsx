12ee397b6c1da1be8f47ec51231535eaf4557632
laurent <88848584+laurentUbik@users.noreply.github.com>
2021-10-15 15:21:36 +0200

    Fix help center lang dropdown (#525)
    * Fix HelpCenter Lang Dropdown
    
    * py
    
    * remove flex
    
    * code review

--- app/javascript/packages/docs/src/site/Dropdown.tsx
+++ app/javascript/packages/docs/src/site/Dropdown.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import { Fragment, useEffect, useRef, useState } from 'react';
 
-export default function Example({ options, icon, filterHandler }) {
+export default function Example({
+  options,
+  label = 'Options',
+  icon,
+  filterHandler,
+}) {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-          Options {icon}
+          <span className={'mr-1'}>{label}</span> {icon}
         </Menu.Button>
       </div>
       <Transition
@@ -19,8 +24,8 @@ export default function Example({ options, icon, filterHandler }) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="px-1 py-1 ">
+        <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
+          <div className="px-1 py-1">
             {options.map((item) => (
               <Menu.Item>
                 {({ active }) => (
@@ -28,9 +33,9 @@ export default function Example({ options, icon, filterHandler }) {
                     onClick={() => filterHandler(item)}
                     className={`${
                       active ? 'bg-violet-500 text-gray-600' : 'text-gray-900'
-                    } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                    } group flex items-center w-full px-2 py-2 text-sm hover:bg-gray-200`}
                   >
-                    {item.name}
+                    <span>{item.name}</span>
                   </button>
                 )}
               </Menu.Item>
