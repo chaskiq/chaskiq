28bd613c460158cd21d7ad858b93ec18a10971c3
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2021-11-09 11:37:39 -0300

    config

--- app/models/config.rb
+++ app/models/config.rb
@@ -1,5 +1,4 @@
 class Config
-
   attr_accessor :config
 
   def initialize(config: {})
@@ -7,12 +6,12 @@ class Config
   end
 
   def get(name)
-    #config[name] || ENV[name.upcase]
+    # config[name] || ENV[name.upcase]
     Rails.application.credentials.config.fetch(name.to_sym, ENV[name.to_s.upcase])
   end
 
   def fetch(name, fallback)
     Rails.application.credentials.config.fetch(name.to_sym, ENV[name.to_s.upcase])
-    #config[name] || ENV.fetch(name.upcase, fallback)
+    # config[name] || ENV.fetch(name.upcase, fallback)
   end
 end
