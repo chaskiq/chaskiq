f1eed3803a05b16517697af09fa21df1db8becaf
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2021-11-26 14:12:48 -0300

    contact validations

--- app/graphql/mutations/app_users/create_app_user.rb
+++ app/graphql/mutations/app_users/create_app_user.rb
@@ -4,6 +4,8 @@ module Mutations
   module AppUsers
     class CreateAppUser < Mutations::BaseMutation
       field :app_user, Types::AppUserType, null: false
+      field :errors, Types::JsonType, null: false
+
       argument :app_key, String, required: true
       argument :options, Types::JsonType, required: true
 
@@ -11,9 +13,10 @@ module Mutations
         app = current_user.apps.find_by(key: app_key)
 
         permitted_options = options.require(:app).permit(
-          %w[name email first_name last_name company]
-        )
+          %w[name email first_name last_name phone company_name]
+        ).to_hash.with_indifferent_access
 
+        permitted_options.merge!({ additional_validations: true })
         case options[:app][:contact_kind]
         when "AppUser"
           app_user = app.add_user(permitted_options)
@@ -27,6 +30,8 @@ module Mutations
       def current_user
         context[:current_user]
       end
+
+      def validate_app_user(user); end
     end
   end
 end
