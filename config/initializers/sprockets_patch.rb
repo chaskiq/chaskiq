2ae2e867ff8d708601a291394bae21041d4400d7
Miguel michelson <miguelmichelson@gmail.com>
2022-02-11 01:30:11 -0300

    patch

--- config/initializers/sprockets_patch.rb
+++ config/initializers/sprockets_patch.rb
@@ -1,6 +1,7 @@
+# remove this when https://github.com/rails/sprockets/pull/726
 Sprockets::DigestUtils.module_eval do |variable|
   def already_digested?(name)
-    cond = name =~ /-([0-9a-f]{7,128})\.digested/ || name =~ /-(?:[A-Z2-7]{8})*(?:[A-Z2-7]{2}={6}|[A-Z2-7]{4}={4}|[A-Z2-7]{5}={3}|[A-Z2-7]{7}=)?\.digested$/
-    puts "DIGESTED NAME: #{name} #{cond ? "SI" : "NO"}"
+    name =~ /-([0-9a-zA-Z]{7,128})\.digested/
+    #puts "DIGESTED NAME: #{name} #{cond ? "SI" : "NO"}"
   end
 end
