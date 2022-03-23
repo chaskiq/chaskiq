e53fc5d81143b0d6248f8f249e22bf7fe0080f87
Miguel michelson <miguelmichelson@gmail.com>
2022-03-23 01:21:57 -0300

    pkg channels on agent sidebar filter, queries added

--- app/javascript/packages/components/src/components/icons.tsx
+++ app/javascript/packages/components/src/components/icons.tsx
@@ -1578,7 +1578,7 @@ export function BillingIcon() {
     </svg>
   );
 }
-export function IntegrationsIcon() {
+export function IntegrationsIcon(props) {
   return (
     <svg
       viewBox="0 0 15 15"
@@ -1586,6 +1586,7 @@ export function IntegrationsIcon() {
       xmlns="http://www.w3.org/2000/svg"
       width="22"
       height="22"
+      className={props.className}
     >
       <path
         d="M7.5 1.5l.197-.46a.5.5 0 00-.394 0l.197.46zm-7 3l-.197-.46a.5.5 0 000 .92L.5 4.5zm7 3l-.197.46a.5.5 0 00.394 0L7.5 7.5zm7-3l.197.46a.5.5 0 000-.92l-.197.46zm-7 6l-.197.46.197.084.197-.084-.197-.46zm0 3l-.197.46.197.084.197-.084-.197-.46zM7.303 1.04l-7 3 .394.92 7-3-.394-.92zm-7 3.92l7 3 .394-.92-7-3-.394.92zm7.394 3l7-3-.394-.92-7 3 .394.92zm7-3.92l-7-3-.394.92 7 3 .394-.92zM.303 7.96l7 3 .394-.92-7-3-.394.92zm7.394 3l7-3-.394-.92-7 3 .394.92zm-7.394 0l7 3 .394-.92-7-3-.394.92zm7.394 3l7-3-.394-.92-7 3 .394.92z"
