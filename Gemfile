6fc28826ef9af1ba79acd82e6210cf2892881450
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2022-04-05 00:48:54 -0400

    Search conversations user (#699)
    * conversation search
    
    * search participant in conversations + fix sidebar

--- Gemfile
+++ Gemfile
@@ -125,8 +125,8 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   # gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem "cypress-on-rails", "~> 1.0"
-  gem "pry"
   gem "execjs"
+  gem "pry"
   # gem "debug", platforms: %i[mri mingw x64_mingw]
 end
 
--- app/graphql/types/app_type.rb
+++ app/graphql/types/app_type.rb
@@ -238,14 +238,14 @@ module Types
                       .where("conversation_channels.provider =?", channel_id)
       end
 
-      @collection = filter_by_agent(agent_id)
+      @collection = filter_by_agent(agent_id) if agent_id.present?
       @collection = @collection.page(page).per(per)
       sort_conversations(sort)
       @collection = @collection.tagged_with(tag) if tag.present?
-      # TODO: add _or_main_participant_name_cont, or do this with Arel
       if term
+        query_term = :main_participant_postal_or_main_participant_phone_or_main_participant_last_name_or_main_participant_first_name_or_main_participant_name_or_messages_messageable_of_ConversationPartContent_type_text_content_i_cont_any
         @collection = @collection.ransack(
-          messages_messageable_of_ConversationPartContent_type_text_content_cont: term
+          query_term => term
         ).result
       end
 
