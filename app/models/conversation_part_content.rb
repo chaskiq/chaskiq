163593efda562cbdcbc1c5af002871fe39ecbb04
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2022-07-20 18:53:18 -0400

    sanitize contents on part save

--- app/models/conversation_part_content.rb
+++ app/models/conversation_part_content.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
 class ConversationPartContent < ApplicationRecord
+  include SanitizeHelpers
+
   has_one :conversation_part, as: :messageable, dependent: :destroy_async
+
+  before_save :sanitize_contents
 
   def as_json(*)
     super.except("created_at",
                  "updated_at",
                  "id",
                  "conversation_part_id")
+  end
+
+  def sanitize_contents
+    self.html_content = sanitize_field(html_content)
+    self.text_content = sanitize_field(text_content)
   end
 
   def parsed_content
