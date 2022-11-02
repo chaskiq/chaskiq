cc5991f9149cd16366c5c2c20d0fba05c4a5b7af
Miguel michelson <miguelmichelson@gmail.com>
2022-11-01 21:36:37 -0300

    fix merge user

--- app/graphql/mutations/app_users/merge_contact.rb
+++ app/graphql/mutations/app_users/merge_contact.rb
@@ -11,7 +11,7 @@ module Mutations
       def resolve(app_key:, from:, to:)
         app = current_user.apps.find_by(app_key: app_key)
         authorize! app, to: :can_manage_users?, with: AppPolicy, context: { app: app }
-        source = app.app_users.find(id)
+        source = app.app_users.find(from)
         destination = app.app_users.find(to)
         app.merge_contact(from: source, destination: to)
         { status: "ok" }
