34355799e1fac80fdfb52ad65f33121eda4803a9
Miguel Michelson <miguelmichelson@gmail.com>
2022-12-06 19:23:22 -0300

    api, avoid external image

--- app/services/message_apis/slack/api.rb
+++ app/services/message_apis/slack/api.rb
@@ -766,7 +766,6 @@ module MessageApis::Slack
 
     # triggered when a new chaskiq message is created
     # will triggered just after the ws notification
-    # rubocop:disable Metrics/PerceivedComplexity
     def notify_message(conversation:, part:, channel:)
       # TODO: ? redis cache here for provider / channel id / part
       return if part.conversation_part_channel_sources.find_by(provider: "slack").present?
@@ -776,8 +775,8 @@ module MessageApis::Slack
       messageable = part.messageable
 
       user_options = {
-        username: format_user_name(part.authorable).to_s,
-        icon_url: part&.authorable&.avatar_url
+        username: format_user_name(part.authorable).to_s
+        # icon_url: part&.authorable&.avatar_url # bug when process external image
       }
 
       if part.messageable.is_a?(ConversationPartBlock)
@@ -805,8 +804,8 @@ module MessageApis::Slack
         user = conversation.main_participant
 
         user_options.merge!({
-                              username: format_user_name(user),
-                              icon_url: user&.avatar_url
+                              username: format_user_name(user)
+                              # icon_url: user&.avatar_url # icon_url: part&.authorable&.avatar_url # bug when process external image
                             })
       end
 
@@ -839,11 +838,6 @@ module MessageApis::Slack
         }]
 
         user = conversation.main_participant
-
-        # user_options.merge!({
-        #  username: format_user_name(user),
-        #  icon_url: user&.avatar_url
-        # })
       end
 
       provider_channel_id = conversation.conversation_channels
@@ -869,7 +863,6 @@ module MessageApis::Slack
         message_source_id: response_data["ts"]
       )
     end
-    # rubocop:enable Metrics/PerceivedComplexity
 
     def format_user_name(user)
       display = [user.display_name, user.email].join(" \u00B7 ")
