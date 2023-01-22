464e6391d792777b719ce2e609b5c0ec822c692b
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2023-01-22 11:06:22 -0300

    Inbound emails check (#905)
    * Bump globalid from 1.0.0 to 1.0.1
    
    Bumps [globalid](https://github.com/rails/globalid) from 1.0.0 to 1.0.1.
    - [Release notes](https://github.com/rails/globalid/releases)
    - [Commits](https://github.com/rails/globalid/compare/v1.0.0...v1.0.1)
    
    ---
    updated-dependencies:
    - dependency-name: globalid
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * inbounds emails check
    
    * format
    
    * fix hook specs
    
    * a spec with empty
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- app/controllers/api/v1/hooks_controller.rb
+++ app/controllers/api/v1/hooks_controller.rb
@@ -191,6 +191,7 @@ class Api::V1::HooksController < ActionController::API
     recipient = mail.recipients.first
     app, agent = decode_inbound_address(recipient)
     return if app.blank?
+    return unless app&.plan&.enabled?("InboundEmails")
 
     create_conversation_from_incoming(app, mail)
   end
@@ -198,6 +199,7 @@ class Api::V1::HooksController < ActionController::API
   def handle_inbound_recipient_from_forwarded(mail, val)
     app, agent = decode_inbound_address(val)
     return if app.blank?
+    return unless app&.plan&.enabled?("InboundEmails")
 
     create_conversation_from_incoming(app, mail)
   end
