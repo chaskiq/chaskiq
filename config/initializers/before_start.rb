71e9534976c97ecb2e48e8d49a886a9d50e396f6
Miguel Michelson <miguelmichelson@gmail.com>
2023-04-10 02:19:21 -0400

    before start will load plugins to the codebase

---
+++ config/initializers/before_start.rb
@@ -0,0 +1,13 @@
+
+
+Rails.application.config.to_prepare do
+  # This will force Rails to load all models
+  Rails.application.eager_load!
+
+  def plugin_autoloader
+    puts "PREPARE TO SAVE CHASKIQ PLUGINS"
+    Plugin.save_all_plugins unless Chaskiq::Config.get("DISABLE_AUTOLOAD_APPSTORE")
+  end
+
+  plugin_autoloader
+end
