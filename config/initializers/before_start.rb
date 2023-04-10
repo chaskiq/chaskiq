028c35965872bb8d4c0fc70eb6e4a5b38ae10299
Miguel Michelson <miguelmichelson@gmail.com>
2023-04-10 02:27:51 -0400

    run command only on rails c & rails s

--- config/initializers/before_start.rb
+++ config/initializers/before_start.rb
@@ -1,13 +1,15 @@
 
 
-Rails.application.config.to_prepare do
-  # This will force Rails to load all models
-  Rails.application.eager_load!
+if defined?(Rails::Server) || defined?(Rails::Console)
+  Rails.application.config.to_prepare do
+    # This will force Rails to load all models
+    Rails.application.eager_load!
 
-  def plugin_autoloader
-    puts "PREPARE TO SAVE CHASKIQ PLUGINS"
-    Plugin.save_all_plugins unless Chaskiq::Config.get("DISABLE_AUTOLOAD_APPSTORE")
+    def plugin_autoloader
+      puts "PREPARE TO SAVE CHASKIQ PLUGINS"
+      Plugin.save_all_plugins unless Chaskiq::Config.get("DISABLE_AUTOLOAD_APPSTORE")
+    end
+
+    plugin_autoloader
   end
-
-  plugin_autoloader
 end
