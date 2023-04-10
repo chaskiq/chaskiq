58dbeb663752b0fcfd99604589a7785d630e7b71
Miguel Michelson <miguelmichelson@gmail.com>
2023-04-10 01:47:56 -0400

    smaller definitions, embedded definitions

--- app/services/message_apis/inbox_sections/api.rb
+++ app/services/message_apis/inbox_sections/api.rb
@@ -3,5 +3,15 @@
 module MessageApis::InboxSections
   class Api < MessageApis::BasePackage
     attr_accessor :secret
+
+    def self.definition_info
+      {
+        name: "InboxSections",
+        description: "Inbox base blocks for conversation sidebar",
+        capability_list: ["inbox"],
+        state: "enabled",
+        definitions: []
+      }
+    end
   end
 end
