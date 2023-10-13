1ea0d977197b5293931b1f4127909f46e6400c13
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2023-10-12 23:33:50 -0300

    update rsepc

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -79,7 +79,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   config.define_derived_metadata(file_path: %r{spec/system}) do |metadata|
     metadata[:browser] = true
