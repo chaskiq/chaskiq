4e82b48b5f6dcc2c9957651af4544f228e8eca7c
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2023-10-25 17:18:09 -0300

    article settings crud

--- app/controllers/apps/articles_settings_controller.rb
+++ app/controllers/apps/articles_settings_controller.rb
@@ -11,12 +11,39 @@ class Apps::ArticlesSettingsController < ApplicationController
     @article_setting = @app.article_settings.presence || @app.build_article_settings
   end
 
+  def new
+    @article_setting = @app.article_settings.presence || @app.build_article_settings
+    render "new", layout: false
+  end
+
   def update
     authorize! @app, to: :can_manage_help_center?, with: AppPolicy, context: {
       app: @app
     }
     @article_setting = @app.article_settings.presence || @app.build_article_settings
-    @article_setting.update(resource_params)
+    
+    new_language = params[:article_setting][:new_language]
+    if new_language.present?
+      flash.now[:notice] = "Laguage added"
+      @article_setting.update(
+        "site_description_#{new_language}": "",
+        "site_title_#{new_language}": ""
+      )
+      @article_setting.translations.last.save
+    else
+      flash.now[:notice] = "Updated"
+      @article_setting.update(resource_params)
+    end
+  end
+
+  def destroy
+    @article_setting = @app.article_settings
+    translation = @article_setting.translations.find_by(locale: params[:lang])
+    translation.destroy if translation.present?
+
+    flash.now[:notice] = "Laguage removed"
+    render "update"
+
   end
 
   private
@@ -26,7 +53,7 @@ class Apps::ArticlesSettingsController < ApplicationController
   end
 
   def resource_params
-    lang_items = @app.translations.map(&:locale).map do |o|
+    lang_items = @article_setting.translations.map(&:locale).map do |o|
       [:"site_title_#{o}", :"site_description_#{o}"]
     end.flatten
 
@@ -41,6 +68,7 @@ class Apps::ArticlesSettingsController < ApplicationController
       :domain,
       :website,
       :google_code,
+      :default_lang,
       lang_items
     )
   end
