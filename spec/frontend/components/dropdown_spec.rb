f0cc0462cc87378187173ddc04653af0df9d441a
Luis Porras <lporras16@gmail.com>
2023-11-02 16:18:56 -0500

    fixing test for dropdown component

--- spec/frontend/components/dropdown_spec.rb
+++ spec/frontend/components/dropdown_spec.rb
@@ -3,7 +3,11 @@
 require "rails_helper"
 
 describe Dropdown::Component do
-  let(:options) { {} }
+  let(:options) do
+    {
+      label: "foo"
+    }
+  end
   let(:component) { Dropdown::Component.new(**options) }
 
   subject { rendered_component }
@@ -11,5 +15,6 @@ describe Dropdown::Component do
   it "renders" do
     render_inline(component)
     expect(page).to have_css("div")
+    expect(page).to have_text(options[:label])
   end
 end
