216ef4014851acbc8e5f72c241e9a962a47ec3a5
Luis Porras <lporras16@gmail.com>
2023-11-03 12:24:49 -0500

    fixing test for pie_chart component

--- spec/frontend/components/pie_chart_spec.rb
+++ spec/frontend/components/pie_chart_spec.rb
@@ -3,13 +3,21 @@
 require "rails_helper"
 
 describe PieChart::Component do
-  let(:options) { {} }
+  let(:label) { "label-chart" }
+  let(:options) do
+    {
+      data: [1, 2, 3],
+      label: label
+    }
+  end
   let(:component) { PieChart::Component.new(**options) }
 
   subject { rendered_component }
 
   it "renders" do
     render_inline(component)
+
     expect(page).to have_css("div")
+    expect(page).to have_text(label.underscore.humanize)
   end
 end
