74a1f7088d4b8f514586185b535e08e3dbad9bae
Luis Porras <lporras16@gmail.com>
2023-11-03 11:34:34 -0500

    fixing tests for hints component

--- spec/frontend/components/hints_spec.rb
+++ spec/frontend/components/hints_spec.rb
@@ -3,13 +3,21 @@
 require "rails_helper"
 
 describe Hints::Component do
-  let(:options) { {} }
+  let(:type) { "platform" }
+  let(:options) do
+    {
+      type: type
+    }
+  end
   let(:component) { Hints::Component.new(**options) }
 
   subject { rendered_component }
 
   it "renders" do
     render_inline(component)
+
     expect(page).to have_css("div")
+    expect(page).to have_text(I18n.t("hints.#{type}.title"))
+    expect(page).to have_text(I18n.t("hints.#{type}.description"))
   end
 end
