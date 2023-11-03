064711adc3bc21a8a3e8812c96c11640ca1d4d29
Luis Porras <lporras16@gmail.com>
2023-11-03 15:07:45 -0500

    fixing tests padder_renderer and modal componentes

--- spec/frontend/components/modal_spec.rb
+++ spec/frontend/components/modal_spec.rb
@@ -3,13 +3,24 @@
 require "rails_helper"
 
 describe Modal::Component do
-  let(:options) { {} }
+  let(:title) { "This is a title" }
+  let(:action_content) { "Modal content" }
+  let(:options) do
+    {
+      title: title,
+      action_content: action_content
+    }
+  end
   let(:component) { Modal::Component.new(**options) }
 
   subject { rendered_component }
 
   it "renders" do
     render_inline(component)
+
     expect(page).to have_css("div")
+    expect(page).to have_text(title)
+    expect(page).to have_text(action_content)
+    expect(page).to have_text(I18n.t("common.cancel"))
   end
 end
