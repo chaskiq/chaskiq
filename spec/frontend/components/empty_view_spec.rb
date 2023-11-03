a7af7eefcda85a7c25a4ecd0a29d7f77d4738f92
Luis Porras <lporras16@gmail.com>
2023-11-03 09:47:07 -0500

    fixing tests for empty_view component

--- spec/frontend/components/empty_view_spec.rb
+++ spec/frontend/components/empty_view_spec.rb
@@ -3,7 +3,14 @@
 require "rails_helper"
 
 describe EmptyView::Component do
-  let(:options) { {} }
+  let(:title) { "This is a title" }
+  let(:subtitle) { "Subtitle" }
+  let(:options) do
+    {
+      subtitle: subtitle,
+      title: title
+    }
+  end
   let(:component) { EmptyView::Component.new(**options) }
 
   subject { rendered_component }
@@ -11,5 +18,8 @@ describe EmptyView::Component do
   it "renders" do
     render_inline(component)
     expect(page).to have_css("div")
+
+    expect(page).to have_text(title)
+    expect(page).to have_text(subtitle)
   end
 end
