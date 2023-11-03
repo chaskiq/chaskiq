cc4b649140e9d03b578b4ca05d2fc068c8c9cf9e
Luis Porras <lporras16@gmail.com>
2023-11-03 15:21:40 -0500

    fixing tests for input_renderer component

--- spec/frontend/components/input_renderer_spec.rb
+++ spec/frontend/components/input_renderer_spec.rb
@@ -3,13 +3,24 @@
 require "rails_helper"
 
 describe InputRenderer::Component do
-  let(:options) { {} }
+  let(:label) { "Foo" }
+  let(:options) do
+    {
+      id: "id_1",
+      label: label,
+      placeholder: "fill with some text",
+      save_state: "",
+      name: "input name"
+    }
+  end
   let(:component) { InputRenderer::Component.new(**options) }
 
   subject { rendered_component }
 
   it "renders" do
     render_inline(component)
+
     expect(page).to have_css("div")
+    expect(page).to have_text(label)
   end
 end
