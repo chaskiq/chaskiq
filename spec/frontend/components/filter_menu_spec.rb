75f7b550d78f070d61452432e5154187a2da160f
Luis Porras <lporras16@gmail.com>
2023-11-03 10:00:09 -0500

    fixing tests for filter_menu component

--- spec/frontend/components/filter_menu_spec.rb
+++ spec/frontend/components/filter_menu_spec.rb
@@ -3,13 +3,33 @@
 require "rails_helper"
 
 describe FilterMenu::Component do
-  let(:options) { {} }
+  let(:title) { "This is a title" }
+  let(:description) { "This is a description"}
+  let(:label) { "foo" }
+  let(:options) do
+    {
+      items: [
+        {
+          href: "https://example.com",
+          data: "value",
+          state: "checked",
+          title: title,
+          description: description
+        }
+      ],
+      label: label
+    }
+  end
   let(:component) { FilterMenu::Component.new(**options) }
 
   subject { rendered_component }
 
   it "renders" do
     render_inline(component)
+
     expect(page).to have_css("div")
+    expect(page).to have_text(title)
+    expect(page).to have_text(description)
+    expect(page).to have_text(label)
   end
 end
