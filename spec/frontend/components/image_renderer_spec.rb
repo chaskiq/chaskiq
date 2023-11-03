f26c45f7b79157085edb36f3726e8fd5b6da2c1f
Luis Porras <lporras16@gmail.com>
2023-11-03 11:39:41 -0500

    fixing tests for image_renderer component

--- spec/frontend/components/image_renderer_spec.rb
+++ spec/frontend/components/image_renderer_spec.rb
@@ -3,13 +3,17 @@
 require "rails_helper"
 
 describe ImageRenderer::Component do
-  let(:options) { {} }
+  let(:url) { "https://examle.com/image.png" }
+  let(:options) { { url: url } }
   let(:component) { ImageRenderer::Component.new(**options) }
 
   subject { rendered_component }
 
   it "renders" do
     render_inline(component)
-    expect(page).to have_css("div")
+    expect(page).to have_css("div.image")
+
+    image = page.find_css("img").first
+    expect(image["src"]).to eq(url)
   end
 end
