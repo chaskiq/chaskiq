0e6c6c7ad00fc7b64fbdff9a2f49a775b6898d4b
Luis Porras <lporras16@gmail.com>
2023-11-03 15:00:27 -0500

    fixing tests for text_area_renderer component

--- spec/frontend/components/text_area_renderer_spec.rb
+++ spec/frontend/components/text_area_renderer_spec.rb
@@ -3,13 +3,29 @@
 require "rails_helper"
 
 describe TextAreaRenderer::Component do
-  let(:options) { {} }
+  let(:label) { "label" }
+  let(:options) do
+    {
+      id: "foo",
+      label: label,
+      placeholder: "",
+      save_state: "",
+      disabled: "",
+      loading: "",
+      name: "",
+      hint: "",
+      errors: "",
+    }
+  end
   let(:component) { TextAreaRenderer::Component.new(**options) }
 
   subject { rendered_component }
 
   it "renders" do
     render_inline(component)
-    expect(page).to have_css("div")
+
+    expect(page).to have_css("div.input-wrapper")
+    expect(page).to have_text(label)
+    expect(page).to have_css("textarea")
   end
 end
