32c93703d2bcd781f3b0479710d8f10abdef942f
Luis Porras <lporras16@gmail.com>
2023-11-03 12:00:07 -0500

    fixing tests for panel component

--- spec/frontend/components/panel_spec.rb
+++ spec/frontend/components/panel_spec.rb
@@ -3,13 +3,30 @@
 require "rails_helper"
 
 describe Panel::Component do
-  let(:options) { {} }
+  let(:title) { "This is a title" }
+  let(:text) { "This is a text" }
+
+  let(:options) do
+    {
+      title: title,
+      text: text,
+      link: {
+        href: "https://example.com",
+        text: "foo"
+      },
+      classes: "hidden"
+    }
+  end
   let(:component) { Panel::Component.new(**options) }
 
   subject { rendered_component }
 
   it "renders" do
     render_inline(component)
+
     expect(page).to have_css("div")
+    expect(page).to have_text(title)
+    expect(page).to have_text(text)
+    expect(page).to have_css("a")
   end
 end
