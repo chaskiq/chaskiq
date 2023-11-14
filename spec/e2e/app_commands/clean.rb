65f6924cdfa991214ab0fa8becbf3022a2a160ec
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2023-11-14 10:36:05 -0300

    add retry mechanism

--- spec/e2e/app_commands/clean.rb
+++ spec/e2e/app_commands/clean.rb
@@ -1,11 +1,20 @@
 if defined?(DatabaseCleaner)
-  # cleaning the database using database_cleaner
-  # DatabaseCleaner.strategy = :truncation
-  DatabaseCleaner[:active_record].strategy = DatabaseCleaner::ActiveRecord::Truncation.new
-  DatabaseCleaner[:active_record].clean
-  # ActiveRecord::Tasks::DatabaseTasks.truncate_all
-  DatabaseCleaner[:redis].clean
-  DatabaseCleaner.clean
+  attempts = 0
+  begin
+    # Your existing database cleaning logic
+    DatabaseCleaner[:active_record].strategy = DatabaseCleaner::ActiveRecord::Truncation.new
+    DatabaseCleaner[:active_record].clean
+    DatabaseCleaner[:redis].clean
+    DatabaseCleaner.clean
+  rescue ActiveRecord::Deadlocked => e
+    attempts += 1
+    if attempts < 3
+      sleep(2) # Sleep for 2 seconds before retrying
+      retry
+    else
+      raise e # Reraise the exception after 3 attempts
+    end
+  end
 else
   logger.warn "add database_cleaner or update cypress/app_commands/clean.rb"
 end
