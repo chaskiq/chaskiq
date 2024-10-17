915536baaf437a48e548195e6f557eec9ac22e95
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2024-10-16 23:35:42 -0300

    remove falcon

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module Chaskiq
 
     config.active_storage.variant_processor = :mini_magick
 
-    config.active_support.isolation_level = :fiber
+    # config.active_support.isolation_level = :fiber
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
